0c30966522fb24106830e8571ddf12b72857fa75
James Estill <JamesEstill@gmail.com>
2007-07-26 20:34:14 +0000

    Removed example shell script batch_blast_shell1.sh

--- scripts/batch_blast_shell1.sh
+++
@@ -1,2 +0,0 @@
-# geterdone1
-./batch_blast.pl -i /scratch/jestill/wheat_in/geterdone1 -o /scratch/jestill/wheat_out/ -c /home/jlblab/jestill/scripts/dawg-paws/batch_blast_full.jcfg -d /db/jlblab/ --logfile /home/jlblab/jestill/geterdone1.log
