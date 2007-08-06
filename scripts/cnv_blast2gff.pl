f6037fdfba74b952bde931d4232a9a8ef1770254
James Estill <JamesEstill@gmail.com>
2007-08-06 16:36:06 +0000

    Good working copy of cnv_blsat2gff.pl established.

--- scripts/cnv_blast2gff.pl
+++ scripts/cnv_blast2gff.pl
@@ -5,27 +5,120 @@
 #                                                           |
 #-----------------------------------------------------------+
 #  AUTHOR: James C. Estill                                  |
-# CONTACT: jestill_at_sourceforge.net                       |
+# CONTACT: JamesEstill_at_gmail.com                         |
 # STARTED: 07/06/2006                                       |
 # UPDATED: 08/06/2007                                       |
 #                                                           |  
 # DESCRIPTION:                                              | 
-# Parse blast from single large contig against a blast db   |
-# of multiple reads. Output is a single gff format file     |
-# suitable for viewing in Apollo.                           |
+# Convert blast output to a Apollo compatible gff file.     |
 #                                                           |
 # USAGE:                                                    |
 #                                                           |
 #-----------------------------------------------------------+
 
-print "The program has started\n";
+=head1 NAME
+
+Name.pl - Short program description. 
+
+=head1 VERSION
+
+This documentation refers to program version 0.1
+
+=head1 SYNOPSIS
+
+ Usage:
+  Name.pl -i InFile -o OutFile
+
+=head1 DESCRIPTION
+
+This is what the program does
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -i,--infile
+
+Path of the input file.
+
+=item -o,--outfile
+
+Path of the output file.
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item --usage
+
+Short overview of how to use program from command line.
+
+=item --help
+
+Show program usage with summary of options.
+
+=item --version
+
+Show program version.
+
+=item --man
+
+Show the full program manual. This uses the perldoc command to print the 
+POD documentation for the program.
+
+=item -q,--quiet
+
+Run the program with minimal output.
+
+=back
+
+=head1 DIAGNOSTICS
+
+The list of error messages that can be generated,
+explanation of the problem
+one or more causes
+suggested remedies
+list exit status associated with each error
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+Names and locations of config files
+environmental variables
+or properties that can be set.
+
+=head1 DEPENDENCIES
+
+Other modules or software that the program is dependent on.
+
+=head1 BUGS AND LIMITATIONS
+
+Any known bugs and limitations will be listed here.
+
+=head1 LICENSE
+
+GNU LESSER GENERAL PUBLIC LICENSE
+
+http://www.gnu.org/licenses/lgpl.html
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
+
+=cut
 
 #-----------------------------+
 # INCLUDES                    |
 #-----------------------------+
-use Getopt::Std;               # Allows to get options from the command line
+use strict;
+use Getopt::Long;
 use Bio::SearchIO;             # Parse BLAST output
-use DBI();                     # Connect to the database
+
+#-----------------------------+
+# PROGRAM VARIABLES           |
+#-----------------------------+
+my $VERSION = "0.1";
 
 #-----------------------------+
 # LOCAL VARIABLES             |
@@ -34,107 +127,75 @@ my $BlastFile = "/home/jestill/projects/asgr/Asgr_c_Net_BigCat/CPAN001/".
     "contig02/contig02_CPAN001.blo";
 my $GFFOut = "/home/jestill/projects/asgr/Asgr_c_Net_BigCat/CPAN001/".
     "contig02/contig02_CPAN001.gff";
-my $MaxE = 0.00001;
-my $MinQryLen = 100;
+my $max_e = 0.00001;
+my $min_len = 100;
+
+# Set variable scope
+my $seqname;
+my $infile;
+my $outfile;
+
+# Booleans
+my $verbose = 0;
+my $help = 0;
+my $quiet = 0;
+my $show_help = 0;
+my $show_usage = 0;
+my $show_man = 0;
+my $show_version = 0;
+my $do_append = 0;
 
 #-----------------------------+
-# DB RELATED VARS             |
-#-----------------------------+ 
-my $DbName = "dbASGR";             # Database name to connect to
-my $tblSrcTable = "tblSeqData";    # Table with src sequence data
-my $tblDatCat = "tblDatCat";       # Table with information about data
-                                   # categories. Can include color
-                                   # and symbol for drawing.
-my $DbUserName = "jestill";
-my $Species = "cen";               # Species to look up id number for
-#my $Species = "pen";
+# COMMAND LINE OPTIONS        |
+#-----------------------------+
+my $ok = GetOptions(# REQUIRED
+		    "i|infile=s"  => \$infile,
+                    "o|outfile=s" => \$outfile,
+		    "n|name=s"    => \$seqname,
+		    # OPTIONS
+		    "verbose"     => \$verbose,
+		    "append"      => \$do_append,
+		    "maxe"        => \$max_e,
+		    "minlen"      => \$min_len,
+		    "usage"       => \$show_usage,
+		    "version"     => \$show_version,
+		    "man"         => \$show_man,
+		    "h|help"      => \$show_help,
+		    "q|quiet"     => \$quiet,);
 
 #-----------------------------+
-# COMMAND LINE VARIABLES      |
+# SHOW REQUESTED HELP         |
 #-----------------------------+
-my %Options;                  # Options hash to hold the options from the command line
-getopts('q', \%Options);      # Get the options from the command line
-my $quiet = $Options{q};
+if ($show_usage) {
+    print_help("");
+}
+
+if ($show_help || (!$ok) ) {
+    print_help("full");
+}
+
+if ($show_version) {
+    print "\n$0:\nVersion: $VERSION\n\n";
+    exit;
+}
+
+if ($show_man) {
+    # User perldoc to generate the man documentation.
+    system("perldoc $0");
+    exit($ok ? 0 : 2);
+}
 
 #-----------------------------+
-# SET VARIABLE SCOPE          |
+# CHECK REQUIRED OPTIONS      |
 #-----------------------------+
-my $HitName;                   # Name of the hit
-
-#-----------------------------+
-# GET USER PASSWORD           |
-#-----------------------------+
-print "\nPassword for $DbUserName\n";
-system('stty', '-echo') == 0 or die "can't turn off echo: $?";
-$DbUserPassword = <STDIN>;
-system('stty', 'echo') == 0 or die "can't turn on echo: $?";
-chomp $DbUserPassword;
-
-#-----------------------------+
-# OPEN OUTPUT FILE            |
-#-----------------------------+
-open (GFFOUT, ">$GFFOut") ||
-    die "Can not open file:\n $GFFOut\n";
-
-my $dbh = DBI->connect("DBI:mysql:database=$DbName;host=localhost",
-		       $DbUserName, $DbUserPassword,
-		       {'RaiseError' => 1});
-
-
-# OUTPUT IN FORM
-#LTR/gypsy	RepeatMasker: AllRep	LTR/gypsy	27676	27772	268	.	.
+if ( (!$infile) || (!$outfile) || (!$seqname) ) {
+    print_help("full");
+}
 
 #-----------------------------------------------------------+
 # MAIN PROGRAM BODY                                         |
 #-----------------------------------------------------------+
-
-my $BlastReport = new Bio::SearchIO ( '-format' => 'blast',
-				      '-file'   => $BlastFile,
-				      '-signif' => $MaxE,
-				      '-min_query_len' => $MinQryLen) ||
-    die "Could not open BLAST input file:\n$BlastFile.\n";
-
-while ($BlastResult = $BlastReport->next_result())
-{
-    	while ( $BlastHit = $BlastResult->next_hit())
-	{
-	    while ($BlastHSP = $BlastHit->next_hsp())
-	    {
-
-		my $HitName = $BlastHit->name();
-		my $Bac = &GetBacName( $HitName, $Species);
-
-		#-----------------------------+
-		# PRINT OUTPUT TO TERMINAL    |
-		#-----------------------------+ 
-		print $BlastHit->name()."\n";
-		print "\t".$BlastHSP->start('query')."\n";
-		print "\t".$BlastHSP->end('query')."\n";
-		print "\t".$Bac."\n";
-
-		#-----------------------------+
-		# PRINT OUTPUT TO GFF FILE    |
-		# WITH BAC DATA               |
-		#-----------------------------+
-		# Changing BLASTN to the Bac Name appears to allow 
-		# these to be drawn on different levels.
-		print GFFOUT $BlastHit->name()."\t".
-		    "$Bac\t".
-		    "$Bac\t".
-		    $BlastHSP->start('query')."\t".
-		    $BlastHSP->end('query')."\t".
-		    #$BlastHSP->evalue()."\t".
-		    # Apollo seems to work better with the 
-		    # bit score then with the evalue
-		    $BlastHSP->score()."\t".
-		    ".\t.\t".
-		    "$Bac\n";
-
-	    } # End of while next hsp
-
-	} # End of while next hit
-} # End of while next result
-
+blast2gff ($infile, $outfile, $do_append, $seqname);
 
 exit;
 
@@ -143,52 +204,167 @@ exit;
 #-----------------------------------------------------------+
 # SUBFUNCTIONS                                              |
 #-----------------------------------------------------------+
-sub GetBacName
-{
-#-----------------------------+
-# GET THE BAC ID FOR THE      |
-# ID NUMBER AND SPECIES NAME  |
-# PASSED TO THE SUBFUNCTION   |
-#-----------------------------+
 
-    my $SpecNum = $_[0];
-    my $SpecName = $_[1];
-    my $result;
+sub blast2gff {
+# CONVERT BLAST TO GFF 
+    
+    # seqname - ID to use in the seqname field
+    # blastin - path to the blast input file
+    # gffout  - path to the gff output file
+    # append  - boolean append data to existing file at gff out
+    my ($blastin, $gffout, $append, $seqname) = @_;
+    my $blastprog;        # Name of the blast program used (blastn, blastx)
+    my $dbname;           # Name of the database blasted
+    my $hitname;          # Name of the hit
+    my $start;            # Start of the feature
+    my $end;              # End of the feature
+    my $strand;           # Strand of the hit
 
-    my $SelectSQL = "SELECT bac FROM ".$tblSrcTable.
-	" WHERE spec_num = '".$SpecNum."'".
-	" AND species = '".$SpecName."'";
-
-    if (! $quiet)
-    {
-	print "\tSQL STATEMENT\n$SelectSQL\n";
+    # Open the BLAST report object
+    my $blast_report = new Bio::SearchIO ( '-format' => 'blast',
+					   '-file'   => $blastin,
+					   '-signif' => $max_e,
+					   '-min_query_len' => $min_len) 
+	|| die "Could not open BLAST input file:\n$blastin.\n";
+    
+    # Open file handle to the gff outfile    
+    if ($append) {
+	open (GFFOUT, ">>$gffout") 
+	    || die "Can not open file:\n $GFFOut\n";
     }
+    else {
+	open (GFFOUT, ">$gffout") 
+	    || die "Can not open file:\n $GFFOut\n";
+    }
+    
+    while (my $blast_result = $blast_report->next_result())
+    {
+	$blastprog = $blast_result->algorithm;
+	$dbname = $blast_result->database_name();
 
-    $dbh->do($SelectSQL);
-    $cur = $dbh->prepare($SelectSQL);
-    $cur->execute();
-    @row=$cur->fetchrow;
-    $result=$row[0] || "SQL ERROR";
-    $cur->finish();
-    return $result;
+    	while (my $blast_hit = $blast_result->next_hit())
+	{
 
+	    while (my $blast_hsp = $blast_hit->next_hsp())
+	    {
+
+		my $hitname = $blast_hit->name();
+
+		$strand = $blast_hsp->strand('query');
+		
+		if ($strand =~ "-1") {
+		    $strand = "-";
+		}
+		elsif ($strand =~ "1") {
+		    $strand = "+";
+		}
+		else {
+		    die "Error parsing strand\n";
+		}
+		
+		# Make certain that start coordinate is
+		# less then the end coordinate
+		if ( $blast_hsp->start() < $blast_hsp->end() ) {
+		    $start = $blast_hsp->start();         # Start
+		    $end = $blast_hsp->end();
+		    #$strand = "+";
+		    
+		}
+		else {
+		    #
+		    $start = $blast_hsp->end();         # Start
+		    $end = $blast_hsp->start();
+		    #$strand = "-";
+		}
+
+
+		#-----------------------------+
+		# PRINT OUTPUT TO TERMINAL    |
+		#-----------------------------+
+		if ($verbose) {
+		    print
+			"$seqname\t".                            # Seqname
+			"$blastprog:$dbname\t".                  # Source
+			"exon\t".                                # Feature type name
+			"$start\t".                              # Start
+			"$end\t".                                # End
+			$blast_hsp->score()."\t".                # Score
+			"$strand\t".
+			".\t".                                   
+			"$hitname\n";                            # Feature name
+		}		
+		
+		#-----------------------------+
+		# PRINT OUTPUT TO GFF FILE    |
+		# WITH BAC DATA               |
+		#-----------------------------+
+		# Changing BLASTN to the Bac Name appears to allow 
+		# these to be drawn on different levels.
+		print GFFOUT 
+		    #$blast_hit->name()."\t".
+		    "$seqname\t".                            # Seqname
+		    "$blastprog:$dbname\t".                  # Source
+		    "exon\t".                                # Feature type name
+		    "$start\t".                              # Start
+		    "$end\t".                                # End
+		    $blast_hsp->score()."\t".                # Score
+		    "$strand\t".                             # Strand
+		    ".\t".                                   # Frame
+		    "$hitname\n";                            # Feature name
+
+	    } # End of while next hsp
+	} # End of while next hit
+    } # End of while next result
+    
+    close GFFOUT;
+    
 }
 
+sub print_help {
+
+    # Print requested help or exit.
+    # Options are to just print the full 
+    my ($opt) = @_;
+
+    my $usage = "USAGE:\n". 
+	"cnv_blast2gff.pl -i InFile -o OutFile\n";
+    my $args = "REQUIRED ARGUMENTS:\n".
+	"  --infile       # Path to the input file\n".
+	"  --outfile      # Path to the output file\n".
+	"\n".
+	"OPTIONS::\n".
+	"  --append       # Append results to an existing file\n".
+	"  --version      # Show the program version\n".     
+	"  --usage        # Show program usage\n".
+	"  --help         # Show this help message\n".
+	"  --man          # Open full program manual\n".
+	"  --quiet        # Run program with minimal output\n";
+	
+    if ($opt =~ "full") {
+	print "\n$usage\n\n";
+	print "$args\n\n";
+    }
+    else {
+	print "\n$usage\n\n";
+    }
+    
+    exit;
+}
+
+
+=head1 HISTORY
+
+STARTED: 08/06/2007
+
+UPDATED: 08/06/2007
+
+VERSION: $Id: $
+
+=cut
 
 #-----------------------------------------------------------+
 # HISTORY                                                   |
 #-----------------------------------------------------------+
 #
-# 07/06/2006
-# - Program written to parse BLAST file and send out to
-#   Apollo compatible GFF formatted file. 
-#
-# 07/07/2006
-# - Added GetBacName subfunction
-# - Changed score used to the BIT Score. This works better
-#   with apollo
-#
 # 08/06/2007
-# - Modifying for high throughput conversion
-
-
+# -Program started
