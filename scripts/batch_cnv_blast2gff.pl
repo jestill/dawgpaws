9ba016bcc7bc530b76856e4ab41c549a1520865e
James Estill <JamesEstill@gmail.com>
2007-12-04 20:06:01 +0000

    KLUDGE Trying to get the podselect and pod2text functions to work well together. Had to resort to pipes within PERL or use pipes from the system command.
    I decided to go with pipes withing PERL. This should be a more standard solution that will work on Win32 machines.

--- scripts/batch_cnv_blast2gff.pl
+++ scripts/batch_cnv_blast2gff.pl
@@ -18,98 +18,6 @@
 #                                                           |
 #-----------------------------------------------------------+
 
-=head1 NAME
-
-batch_cnv_blast2gff.pl - Convert blast output to gff format
-
-=head1 VERSION
-
-This documentation refers to program version $Rev$
-
-=head1 SYNOPSIS
-
- Usage:
-  Name.pl -i InDir -o OutDir
-
-=head1 DESCRIPTION
-
-This is what the program does
-
-=head1 REQUIRED ARGUMENTS
-
-=over 2
-
-=item -i,--infile
-
-Path of the input file.
-
-=item -o,--outfile
-
-Path of the output file.
-
-=back
-
-=head1 OPTIONS
-
-=over 2
-
-=item --usage
-
-Short overview of how to use program from command line.
-
-=item --help
-
-Show program usage with summary of options.
-
-=item --version
-
-Show program version.
-
-=item --man
-
-Show the full program manual. This uses the perldoc command to print the 
-POD documentation for the program.
-
-=item -q,--quiet
-
-Run the program with minimal output.
-
-=back
-
-=head1 DIAGNOSTICS
-
-The list of error messages that can be generated,
-explanation of the problem
-one or more causes
-suggested remedies
-list exit status associated with each error
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Names and locations of config files
-environmental variables
-or properties that can be set.
-
-=head1 DEPENDENCIES
-
-Other modules or software that the program is dependent on.
-
-=head1 BUGS AND LIMITATIONS
-
-Any known bugs and limitations will be listed here.
-
-=head1 LICENSE
-
-GNU LESSER GENERAL PUBLIC LICENSE
-
-http://www.gnu.org/licenses/lgpl.html
-
-=head1 AUTHOR
-
-James C. Estill E<lt>JamesEstill at gmail.comE<gt>
-
-=cut
-
 #-----------------------------+
 # INCLUDES                    |
 #-----------------------------+
@@ -117,6 +25,14 @@ use strict;                    # Follow the rules
 use Getopt::Long;              # Get options from the command line
 use Bio::SearchIO;             # Parse BLAST output
 use File::Copy;                # Copy the gff output to the gff dir
+use Pod::Select;               # Print subsections of POD documentation
+#use Pod::Text;                 # Print POD doc as formatted text file
+use IO::String;
+use IO::Scalar;
+use IO::File;
+use IO::Pipe;
+use Pod::Html qw( &pod2html );
+use Pod::Text qw( &pod2text );
 
 #-----------------------------+
 # PROGRAM VARIABLES           |
@@ -165,7 +81,7 @@ my $file_num = 0;
 #-----------------------------+
 my $ok = GetOptions(# REQUIRED
 		    "i|indir=s"   => \$indir,
-                    "o|outdir=s"   => \$outdir,
+                    "o|outdir=s"  => \$outdir,
 		    # OPTIONS
 		    "logfile=s"   => \$logfile,
 		    # BOOLEANS
@@ -182,8 +98,90 @@ my $ok = GetOptions(# REQUIRED
 #-----------------------------+
 # SHOW REQUESTED HELP         |
 #-----------------------------+
+#if ($show_usage) {
+#    print_help("");
+#}
+
+
+# Printing Help with POD::Select
 if ($show_usage) {
-    print_help("");
+    
+    print "\n";
+    
+    #-----------------------------+
+    # PIPE WITHIN PERL            |
+    #-----------------------------+
+    # This code made possible by:
+    # http://www.perlmonks.org/index.pl?node_id=76409
+    # Tie info developed on:
+    # http://www.perlmonks.org/index.pl?node=perltie 
+    #
+    my $podfile = $0;
+    my $scalar = '';
+    tie *STDOUT, 'IO::Scalar', \$scalar;
+    podselect({-sections => ["NAME|SYNOPSIS|MORE"]}, $0);
+    untie *STDOUT;
+    # now $scalar contains the pod from $podfile you can see this below
+    #print $scalar;
+
+    my $pipe = IO::Pipe->new()
+	or die "failed to create pipe: $!";
+    
+    my ($pid,$fd);
+
+    if ( $pid = fork() ) { #parent
+	open(TMPSTDIN, "<&STDIN")
+	    or die "failed to dup stdin to tmp: $!";
+	$pipe->reader();
+	$fd = $pipe->fileno;
+	open(STDIN, "<&=$fd")
+	    or die "failed to dup \$fd to STDIN: $!";
+	# BEGIN AT WORK HERE
+	#pod2html();	
+	pod2text();
+	# END AT WORK HERE
+	open(STDIN, "<&TMPSTDIN")
+	    or die "failed to restore dup'ed stdin: $!";
+    }
+    else { #child
+	$pipe->writer();
+	$pipe->print($scalar);
+	$pipe->close();	
+	exit 0;
+    }
+    
+    $pipe->close();
+
+    #-----------------------------+
+    # PRINT POD SECTION           |
+    #-----------------------------+
+    # The following prints NAME and SYNOPSIS
+    #podselect({-sections => ["NAME|SYNOPSIS|MORE"]}, $0);
+
+    #-----------------------------+
+    # PRINT ENTIRE POD
+    #-----------------------------+
+    #podselect ($0);
+
+    #-----------------------------+
+    # PRINT RAW POD               |
+    # OBJ ORIENTED                |
+    #-----------------------------+
+    #my $parser = new Pod::Select(-output => ">&STDERR");
+    #my $pod_txt = Pod::Text->new (sentence => 0, width => 78);
+    #$parser->select("NAME|SYNOPSIS|BLANK");
+    #$parser->parse_from_file($0);
+
+    #-----------------------------+
+    # USE SYSTEM COMMANDS         |
+    #-----------------------------+
+    #my $usage_cmd = "podselect -section 'SYNOPSIS|MORE' $0 | pod2text";
+    #system ($usage_cmd);
+
+    print "\n";
+
+    exit 0;
+
 }
 
 if ($show_help || (!$ok) ) {
@@ -289,6 +287,12 @@ for my $ind_file (@fasta_files)
     }  
     elsif ($ind_file =~ m/(.*)\.fasta$/ ) {	    
 	$name_root = "$1";
+    }
+    if ($ind_file =~ m/(.*)\.masked\.fa$/ ) {	    
+	$name_root = "$1";
+    }
+    elsif ($ind_file =~ m/(.*)\.fa$/ ) {	    
+	$name_root = "$1";
     } 
     else {
 	$name_root = "UNDEFINED";
@@ -298,11 +302,14 @@ for my $ind_file (@fasta_files)
     print LOG "\n\nProcessing File $file_num of $num_files.\n" if $logfile;
 
     # PRINT PROCESS STATUS TO TERMINAL
-    print "\n\n+-----------------------------------------------------------+\n"
-	unless $quiet;
-    print "| Processing File $file_num of $num_files.\n" unless $quiet;
-    print "+-----------------------------------------------------------+\n"
-	unless $quiet;
+    print STDERR 
+	"\n\n+-----------------------------------------------------------+\n"
+	if $verbose;
+    print STDERR 
+	"| Processing File $file_num of $num_files.\n" if $verbose;
+    print STDERR 
+	"+-----------------------------------------------------------+\n"
+	if $verbose;
     
     #-----------------------------+
     # LOAD LIST OF BLAST FILES    |
@@ -331,9 +338,9 @@ for my $ind_file (@fasta_files)
 	$out_gff_copy = $out_gff_dir.$name_root."_all_blast.gff";
 
 	
-	print "IN:\t$ind_blast_dir\n";
-	print "OUT:\t$out_gff_path\n";
-	print "COPY:\t$out_gff_copy\n\n";
+	print STDERR "IN:\t$ind_blast_dir\n";
+	print STDERR "OUT:\t$out_gff_path\n";
+	print STDERR "COPY:\t$out_gff_copy\n\n";
 
 	#-----------------------------+
 	# CONVERT EACH BLAST OUTFILE  |
@@ -356,7 +363,8 @@ for my $ind_file (@fasta_files)
 	    my $blast_file_path = $ind_blast_dir.$ind_blast_file;
 	    print "Converting: $ind_blast_file\n";
 
-	    blast2gff ( $blast_file_path, $out_gff_path, $do_append, $name_root);
+	    blast2gff ( $blast_file_path, $out_gff_path, 
+			$do_append, $name_root);
 	}
 
 	
@@ -368,7 +376,7 @@ for my $ind_file (@fasta_files)
 	    "\t$out_gff_path\n".
 	    "\t$out_gff_copy\n";
 	copy ($out_gff_path, $out_gff_copy)
-	    || print LOG "msg";
+	    || print LOG "$msg";
 
     }
     else {
@@ -497,8 +505,8 @@ sub print_help {
     my $usage = "USAGE:\n". 
 	"cnv_blast2gff.pl -i InFile -o OutFile\n";
     my $args = "REQUIRED ARGUMENTS:\n".
-	"  --infile       # Path to the input file\n".
-	"  --outfile      # Path to the output file\n".
+	"  --indir       # Path to the input directory\n".
+	"  --outdir      # Path to the output direcotry\n".
 	"\n".
 	"OPTIONS::\n".
 	"  --append       # Append results to an existing file\n".
@@ -519,6 +527,187 @@ sub print_help {
     exit;
 }
 
+1;
+__END__
+
+=head1 NAME
+
+batch_cnv_blast2gff.pl - Convert blast output to gff format
+
+=head1 VERSION
+
+This documentation refers to program version $Rev$
+
+=head1 SYNOPSIS
+
+=head2 Usage
+
+    batch_cnv_blast2gff.pl -i InDir -o OutDir
+
+=head2 Required Arguments
+
+    -i, --indir    # Directory of fasta files to process
+    -o, --outdir   # Path to the base output directory
+
+=head1 DESCRIPTION
+
+Convert NCBI BLAST output to an Apollo compatible GFF file. This can produce
+a separate gff file for each BLAST report, or can merge all BLAST results
+into a single GFF file.
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -i,--indir
+
+Path of the directory containing the sequences to process.
+
+=item -o,--outdir
+
+Path of the directory to place the program output.
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item --logfile
+
+Path to a file that will be used to log program status.
+If the file already exists, additional information will be concatenated
+to the existing file
+
+=item --append
+
+Append results to existing gff file.
+
+=item --usage
+
+Print a short overview of how to use program from the command line.
+
+=item --help
+
+Print a short program usage state with a summary of options.
+
+=item --version
+
+Show program version. This will print the SVN version of the script.
+
+=item --man
+
+Show the full program manual. This uses the perldoc command to print the 
+POD documentation for the program.
+
+=item -q,--quiet
+
+Run the program with minimal output.
+
+=item --verbose
+
+Run the program with maximum output to the screen. This will provided a 
+detailed status of the progress of the program.
+
+=back
+
+=head1 DIAGNOSTICS
+
+Error messages generated by this program and possible solutions are listed
+below.
+
+=over 2
+
+=item ERROR: No fasta files were found in the input directory
+
+The input directory does not contain fasta files in the expected format.
+This could happen because you gave an incorrect path or because your sequence 
+files do not have the expected *.fasta extension in the file name.
+
+=item ERROR: Could not create the output directory
+
+The output directory could not be created at the path you specified. 
+This could be do to the fact that the directory that you are trying
+to place your base directory in does not exist, or because you do not
+have write permission to the directory you want to place your file in.
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This program does not depend on any configuration files or environmental
+settings.
+
+=head1 DEPENDENCIES
+
+=head2 Required Perl Modules
+
+=over
+
+=item * Bio::SearchIO;
+
+This module is part of the BioPerl package of programs. It is used to
+parse the BLAST output. For information on downloading and installing
+BioPerl see http://www.bioperl.org/wiki/Main_Page
+
+=item * File::Copy
+
+This module is required to copy the BLAST results.
+
+=item * Getopt::Long
+
+This module is required to accept options at the command line.
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+=head2 Limitations
+
+=over
+
+=item * Limited to NCBI BLAST
+
+The current version is limited to using the NCBI version of BLAST.
+
+=item * Limited file extensions are supported
+
+BLAST output file must currently end with blo, bln, or blx. For example
+a BLASTx output may be named BlastOut.blx while a BLASTN output
+may be names BlastOut.bln. FASTA files must end with a fasta or fa extension.
+For examples must have names like my_seq.fasta or my_seq.fa.
+
+=back
+
+=head1 SEE ALSO
+
+The batch_blast.pl program is part of the DAWG-PAWS package of genome
+annotation programs. See the DAWG-PAWS web page 
+( http://dawgpaws.sourceforge.net/ )
+or the Sourceforge project page 
+( http://sourceforge.net/projects/dawgpaws ) 
+for additional information about this package.
+
+=head1 LICENSE
+
+GNU GENERAL PUBLIC LICENSE, VERSION 3
+
+http://www.gnu.org/licenses/gpl.html   
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
 =head1 HISTORY
 
@@ -526,7 +715,7 @@ STARTED: 08/06/2007
 
 UPDATED: 08/06/2007
 
-VERSION: $Id: $
+VERSION: $Rev$
 
 =cut
 
@@ -535,4 +724,15 @@ VERSION: $Id: $
 #-----------------------------------------------------------+
 #
 # 08/06/2007
-# -Program started
+# -Program started, all base subfunctions added
+#
+# 12/04/2007
+# - POD Documentation updated
+# - Version number changed to SVN Revision Number
+#
+# 12/04/2007
+# - Changed terminal output to STDERR
+# - Moved POD documentation to end of the code
+# - Trying to add POD select to print help and usage
+#   message from the POD documentation
+# - Addd an end statement
