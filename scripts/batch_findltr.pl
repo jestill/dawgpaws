5aae205bdac6126003e3224a634e8731241d5a6a
James Estill <JamesEstill@gmail.com>
2007-12-10 18:10:28 +0000

    Modified batch_findltr.pl:  - Updated POD documentation  - Converted to new help subfunction that extracts help    and usage message from the POD documentation

--- scripts/batch_findltr.pl
+++ scripts/batch_findltr.pl
@@ -29,6 +29,12 @@ package DAWGPAWS;
 use strict;
 use Getopt::Long;
 use File::Copy;
+# The following needed for printing help
+use Pod::Select;               # Print subsections of POD documentation
+use Pod::Text;                 # Print POD doc as formatted text file
+use IO::Scalar;                # For print_help subfunction
+use IO::Pipe;                  # Pipe for STDIN, STDOUT for POD docs
+use File::Spec;                # Convert a relative path to an abosolute path
 
 #-----------------------------+
 # PROGRAM VARIABLES           |
@@ -89,12 +95,14 @@ my $ok = GetOptions(# REQUIRED OPTIONS
 #-----------------------------+
 # SHOW REQUESTED HELP         |
 #-----------------------------+
-if ($show_usage) {
-    print_help("");
+if ( ($show_usage) ) {
+#    print_help ("usage", File::Spec->rel2abs($0) );
+    print_help ("usage", $0 );
 }
 
-if ($show_help || (!$ok) ) {
-
+if ( ($show_help) || (!$ok) ) {
+#    print_help ("help",  File::Spec->rel2abs($0) );
+    print_help ("help",  $0 );
 }
 
 if ($show_version) {
@@ -114,7 +122,7 @@ if ( (!$indir) || (!$outdir) || (!$config_file) ) {
     print STDERR "ERROR: An input directory must be specified" if !$indir;
     print STDERR "ERROR: An output directory must be specified" if !$outdir;
     print STDERR "ERROR: A config file must be specified" if !$config_file;
-    print_help("full");
+    print_help ("help",  $0 );
     exit;
 }
 
@@ -362,34 +370,68 @@ exit;
 #-----------------------------------------------------------+
 
 sub print_help {
-
-    # Print requested help or exit.
-    # Options are to just print the full 
-    my ($opt) = @_;
-
-    my $usage = "USAGE:\n". 
-	"MyProg.pl -i InFile -o OutFile";
-    my $args = "REQUIRED ARGUMENTS:\n".
-	"  --infile       # Path to the input file\n".
-	"  --outfile      # Path to the output file\n".
-	"\n".
-	"OPTIONS::\n".
-	"  --version      # Show the program version\n".     
-	"  --usage        # Show program usage\n".
-	"  --help         # Show this help message\n".
-	"  --man          # Open full program manual\n".
-	"  --quiet        # Run program with minimal output\n";
-	
-    if ($opt =~ "full") {
-	print "\n$usage\n\n";
-	print "$args\n\n";
+    my ($help_msg, $podfile) =  @_;
+    # help_msg is the type of help msg to use (ie. help vs. usage)
+    
+    print "\n";
+    
+    #-----------------------------+
+    # PIPE WITHIN PERL            |
+    #-----------------------------+
+    # This code made possible by:
+    # http://www.perlmonks.org/index.pl?node_id=76409
+    # Tie info developed on:
+    # http://www.perlmonks.org/index.pl?node=perltie 
+    #
+    #my $podfile = $0;
+    my $scalar = '';
+    tie *STDOUT, 'IO::Scalar', \$scalar;
+    
+    if ($help_msg =~ "usage") {
+	podselect({-sections => ["SYNOPSIS|MORE"]}, $0);
     }
     else {
-	print "\n$usage\n\n";
+	podselect({-sections => ["SYNOPSIS|ARGUMENTS|OPTIONS|MORE"]}, $0);
+    }
+
+    untie *STDOUT;
+    # now $scalar contains the pod from $podfile you can see this below
+    #print $scalar;
+
+    my $pipe = IO::Pipe->new()
+	or die "failed to create pipe: $!";
+    
+    my ($pid,$fd);
+
+    if ( $pid = fork() ) { #parent
+	open(TMPSTDIN, "<&STDIN")
+	    or die "failed to dup stdin to tmp: $!";
+	$pipe->reader();
+	$fd = $pipe->fileno;
+	open(STDIN, "<&=$fd")
+	    or die "failed to dup \$fd to STDIN: $!";
+	my $pod_txt = Pod::Text->new (sentence => 0, width => 78);
+	$pod_txt->parse_from_filehandle;
+	# END AT WORK HERE
+	open(STDIN, "<&TMPSTDIN")
+	    or die "failed to restore dup'ed stdin: $!";
+    }
+    else { #child
+	$pipe->writer();
+	$pipe->print($scalar);
+	$pipe->close();	
+	exit 0;
     }
     
-    exit;
+    $pipe->close();
+    close TMPSTDIN;
+
+    print "\n";
+
+    exit 0;
+   
 }
+
 
 sub findltr2gff {
 
@@ -525,6 +567,8 @@ sub findltr2gff {
 
 } # End of findltr2gff
 
+1;
+__END__
 
 =head1 NAME
 
@@ -536,34 +580,23 @@ This documentation refers to program version $Rev$
 
 =head1 SYNOPSIS
 
-  USAGE:
+=head2 Usage
+
     batch_findltr.pl -i InFile -o OutFile -c Config.cfg [--gff]
 
-  REQUIRED ARGUMENTS
+=head2 Required Arguments
+
     --indir         # Path to the input directory of fasta files
     --outdir        # Path to the base output directory
     --config        # Config file containg batch_findltr.pl paramaters
   
-  ADDITIONAL OPTIONS
-    --gff           # Produce a gff formatted output of the results
-    --quiet         # Run the program in quiet mode
-    --verbose       # Run the program with maximum output to STDERR
-  
-  ADDITIONAL PROGRAM INFORMATION
-    --usage         # Show basic program usage
-    --help          # Show program synopsis
-    --version       # Show program version and exit
-    --man           # Show the full program manual
- 
 =head1 DESCRIPTION
 
 Runs the program find_ltr.pl in batch mode. This makes use of a modified
 version of the find_ltr.pl program that takes changes to the LTR finding
 parameters at the command line.
 
-=head1 COMMAND LINE ARGUMENTS
-
-=head2 Required Arguments
+=head1 REQUIRED ARGUMENTS
 
 =over 2
 
@@ -595,7 +628,7 @@ environment heading below.
 
 =back
 
-=head2 Additional Options
+=head1 OPTIONS
 
 =over 2
 
@@ -622,12 +655,6 @@ Run the program in test mode. The find_ltr.pl program will not be run, but
 the location of source files, binaries, will be checked and the 
 outupt directories will be created.
 
-=back
-
-=head2 Additional Program Information
-
-=over 2
-
 =item --usage
 
 Short overview of how to use program from command line.
@@ -647,30 +674,32 @@ POD documentation for the program.
 
 =back
 
+
 =head1 DIAGNOSTICS
 
-The list of error messages that can be generated,
-explanation of the problem
-one or more causes
-suggested remedies
-list exit status associated with each error
+Error messages generated by this program and possible solutions are listed
+below.
+
+=over 2
+
+=item ERROR: No fasta files were found in the input directory
+
+The input directory does not contain fasta files in the expected format.
+This could happen because you gave an incorrect path or because your sequence 
+files do not have the expected *.fasta extension in the file name.
+
+=item ERROR: Could not create the output directory
+
+The output directory could not be created at the path you specified. 
+This could be do to the fact that the directory that you are trying
+to place your base directory in does not exist, or because you do not
+have write permission to the directory you want to place your file in.
+
+=back
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-B<FIND_LTR_PATH Environment>
-
-As an alternative to specifying the full path of the find_ltr program
-with the --fl-path option,
-the path of the find_ltr program can be specified in the users environment.
-For example in bash shell, add the following line to your .bashrc
-
-  export FIND_LTR_PATH='/usr/local/genome/find_ltr.pl'
-
-assuming that the find_ltr.pl program is in the /usr/local/genome/
-directory. This allows the find_ltr.pl program location to be at 
-any specific location .
-
-B<Configuartion File>
+=head2 Configuration File
 
 The configuration file in batch_findltr.pl specifies the options for
 running the find_ltr.pl program. This is a white space delimited text
@@ -731,22 +760,93 @@ Mac E value of HMM Hit
 
 =back
 
+=head2 FIND_LTR_PATH Environment
+
+As an alternative to specifying the full path of the find_ltr program
+with the --fl-path option,
+the path of the find_ltr program can be specified in the users environment.
+For example in bash shell, add the following line to your .bashrc
+
+  export FIND_LTR_PATH='/usr/local/genome/find_ltr.pl'
+
+assuming that the find_ltr.pl program is in the /usr/local/genome/
+directory.
+
 =head1 DEPENDENCIES
 
-B<find_ltr.pl>
+=head2 Required Software
+
+=over
+
+=item * find_ltr.pl>
 
 A modified version of the find_ltr.pl program is required.
 
+=back
+
+
+=head2 Required Perl Modules
+
+=over
+
+=item * File::Copy
+
+This module is required to copy the BLAST results.
+
+=item * Getopt::Long
+
+This module is required to accept options at the command line.
+
+=back
+
 =head1 BUGS AND LIMITATIONS
+
+
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+
+=head2 Limitations
+
+=over
+
+=item * Modified version of findltr required
 
 The batch_finltr.pl program requies a modified version of the batch_ltr.pl
 program that accepts parameters from the command line.
 
+=item * Config file must use UNIX format line endings
+
+The config file must have UNIX formatted line endings. Because of
+this any config files that have been edited in programs such as
+MS Word must be converted to a UNIX compatible text format before
+being used with batch_blast.
+
+=back
+
+
+=head1 SEE ALSO
+
+The batch_blast.pl program is part of the DAWG-PAWS package of genome
+annotation programs. See the DAWG-PAWS web page 
+( http://dawgpaws.sourceforge.net/ )
+or the Sourceforge project page 
+( http://sourceforge.net/projects/dawgpaws ) 
+for additional information about this package.
+
 =head1 LICENSE
 
-GNU LESSER GENERAL PUBLIC LICENSE
+GNU GENERAL PUBLIC LICENSE, VERSION 3
 
-http://www.gnu.org/licenses/lgpl.html
+http://www.gnu.org/licenses/gpl.html   
 
 =head1 AUTHOR
 
@@ -756,7 +856,7 @@ James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
 STARTED: 09/13/2007
 
-UPDATED: 09/14/2007
+UPDATED: 12/10/2007
 
 VERSION: $Rev$
 
@@ -771,3 +871,7 @@ VERSION: $Rev$
 # - Added findltr2gff from cnv_findltr2gff.pl 
 # 09/28/2007
 # - Making gff output type column SONG complient
+# 12/10/2007
+# - Updated POD documentation
+# - Converted to new help subfunction that extracts help
+#   and usage message from the POD documentation
