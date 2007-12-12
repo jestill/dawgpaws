e1288bd02e4fadd1e4c70409132dbdf4d58bde5c
James Estill <JamesEstill@gmail.com>
2007-12-12 21:25:24 +0000

    Update fetch_tenest.pl # - Moved POD documentation to the end of the script # - Added print_help subfunction to extract help and #   usage statements from the POD documentation # - Updated POD documentation # - Changed --infile to --config

--- scripts/fetch_tenest.pl
+++ scripts/fetch_tenest.pl
@@ -8,7 +8,7 @@
 #  AUTHOR: James C. Estill                                  |
 # CONTACT: JamesEstill_@_gmail.com                          |
 # STARTED: 08/29/2007                                       |
-# UPDATED: 08/29/2007                                       |
+# UPDATED: 12/12/2007                                       |
 #                                                           |
 # DESCRIPTION:                                              |
 #  Workflow to fetch the tenest program results and covert  |
@@ -16,119 +16,16 @@
 #  pipeline.                                                |
 #                                                           |
 # VERSION:                                                  |
-# $Id:: fetch_tenest.pl 85 2007-08-29 14:29:27Z JamesEst#$: |
+# $Rev$                                                     |
 #                                                           |
 # LICENSE:                                                  |
 #  GNU General Public License, Version 3                    |
 #  http://www.gnu.org/licenses/gpl.html                     |  
 #                                                           |
 #-----------------------------------------------------------+
-# TODO: Update POD
-#       Fix help function
-#       Add dir to the tenest2gff subfunction
-
-=head1 NAME
-
-fetch_tenest.pl - Fetch TE Nest LTR and SVG files
-
-=head1 VERSION
-
-This documentation refers to $Rev$
-
-=head1 SYNOPSIS
-
-  USAGE:
-    fetch_tenest.pl -i SeqList.txt -o OutDir [--gff]
-
-=head1 DESCRIPTION
-
-Given a tab delimited input file of theFetches the TE Nest results using wget.
-The results are converted to gff and copied to the gff dir if requested.
-
-=head1 REQUIRED ARGUMENTS
-
-=over 2
-
-=item -i,--infile
-
-Path of the input file. This is a tab delimited input file that must be
-in the following format:
-
-    HEX0350E24     1188334554_18873
-    HEX0358K19     1188334578_19208
-    HEX0411K13     1188334604_19821
-
-Where the first column is the name of the sequence that was analyzed
-by TE Nest and the second column is the id of the TE Nest job. This job 
-id is returned by TE Nest when the job is initially submitted.
-
-=item -o,--outdir
-
-Path of the output directory. This is the base dir that the analysis 
-results are stored in. 
-
-=back
-
-=head1 OPTIONS
-
-=over 2
-
-=item --usage
-
-Short overview of how to use program from command line.
-
-=item --help
-
-Show program usage with summary of options.
-
-=item --version
-
-Show program version.
-
-=item --man
-
-Show the full program manual. This uses the perldoc command to print the 
-POD documentation for the program.
-
-=item -q,--quiet
-
-Run the program with minimal output.
-
-=back
-
-=head1 DIAGNOSTICS
-
-The list of error messages that can be generated,
-explanation of the problem
-one or more causes
-suggested remedies
-list exit status associated with each error
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Names and locations of config files
-environmental variables
-or properties that can be set.
-
-=head1 DEPENDENCIES
-
-Other modules or software that the program is dependent on.
-
-=head1 BUGS AND LIMITATIONS
-
-Any known bugs and limitations will be listed here.
-
-=head1 LICENSE
-
-GNU LESSER GENERAL PUBLIC LICENSE
-
-http://www.gnu.org/licenses/lgpl.html
-
-=head1 AUTHOR
-
-James C. Estill E<lt>JamesEstill at gmail.comE<gt>
-
-=cut
+# TODO:
+#  - Add dir to the tenest2gff subfunction
+#  - Change infile option to config file option
 
 package DAWGPAWS;
 
@@ -139,6 +36,12 @@ use strict;                    # Sanity checker
 use File::Copy;                # Copy files 
 use Getopt::Long;              # Get options from the command line
 use LWP::Simple;               # Used to get files
+# The following needed for printing help
+use Pod::Select;               # Print subsections of POD documentation
+use Pod::Text;                 # Print POD doc as formatted text file
+use IO::Scalar;                # For print_help subfunction
+use IO::Pipe;                  # Pipe for STDIN, STDOUT for POD docs
+use File::Spec;                # Convert a relative path to an abosolute path
 
 #-----------------------------+
 # PROGRAM VARIABLES           |
@@ -174,7 +77,7 @@ my $num_files;                     # The number of files to process
 # COMMAND LINE OPTIONS        |
 #-----------------------------+
 my $ok = GetOptions(# Required variables
-		    "i|infile=s"  => \$infile,
+		    "c|config=s" => \$infile,
                     "o|outdir=s" => \$outdir,
 		    # Additional options
 		    "q|quiet"     => \$quiet,
@@ -190,12 +93,14 @@ my $ok = GetOptions(# Required variables
 #-----------------------------+
 # SHOW REQUESTED HELP         |
 #-----------------------------+
-if ($show_usage) {
-    print_help("");
+if ( ($show_usage) ) {
+#    print_help ("usage", File::Spec->rel2abs($0) );
+    print_help ("usage", $0 );
 }
 
-if ($show_help || (!$ok) ) {
-    print_help("full");
+if ( ($show_help) || (!$ok) ) {
+#    print_help ("help",  File::Spec->rel2abs($0) );
+    print_help ("help",  $0 );
 }
 
 if ($show_version) {
@@ -209,6 +114,20 @@ if ($show_man) {
     exit($ok ? 0 : 2);
 }
 
+
+#-----------------------------+
+# CHECK REQUIRED ARGS         |
+#-----------------------------+
+if ( (!$infile) || (!$outdir) ) {
+    print "\a";
+    print STDERR "\n";
+    print STDERR "ERROR: An input file was not specified at the".
+	" command line\n" if (!$infile);
+    print STDERR "ERROR: An output directory was specified at the".
+	" command line\n" if (!$outdir);
+    print_help ("usage", $0 );
+}
+
 #-----------------------------+
 # CHECK FOR SLASH IN DIR      |
 # VARIABLES                   |
@@ -216,7 +135,6 @@ if ($show_man) {
 unless ($outdir =~ /\/$/ ) {
     $outdir = $outdir."/";
 }
-
 
 #-----------------------------+
 # CREATE THE OUT DIR          |
@@ -237,7 +155,7 @@ unless (-e $outdir) {
 # TAB DELIM TEXT FILE         |
 #-----------------------------+
 open (INFILE, "<$infile")
-    || die "Can not open the input file:\n$infile\n";
+    || die "Can not open the input configuration file:\n$infile\n";
 
 while (<INFILE>) {
     chomp;
@@ -312,7 +230,7 @@ for ($i=0; $i<$num_files; $i++) {
     my $tenest_dir = $name_root_dir."tenest/";
     unless (-e $tenest_dir) {
 	mkdir $tenest_dir ||
-	    die "Could not create genscan out dir:\n$tenest_dir\n";
+	    die "Could not create tenest out dir:\n$tenest_dir\n";
     }
 
     #-----------------------------+
@@ -322,7 +240,7 @@ for ($i=0; $i<$num_files; $i++) {
     my $gff_dir = $name_root_dir."gff/";
     unless (-e $gff_dir) {
 	mkdir $gff_dir ||
-	    die "Could not create genscan out dir:\n$gff_dir\n";
+	    die "Could not create gff out dir:\n$gff_dir\n";
     }
 
     #-----------------------------+
@@ -410,35 +328,67 @@ exit;
 #-----------------------------------------------------------+
 
 sub print_help {
-
-    # Print requested help or exit.
-    # Options are to just print the full 
-    my ($opt) = @_;
-
-    my $usage = "USAGE:\n". 
-	"fetch_tenest.pl -i InFile -o OutFile";
-    my $args = "REQUIRED ARGUMENTS:\n".
-	"  --infile       # Path to the input file\n".
-	"  --outfile      # Path to the output file\n".
-	"\n".
-	"OPTIONS:\n".
-	"  --version      # Show the program version\n".     
-	"  --usage        # Show program usage\n".
-	"  --help         # Show this help message\n".
-	"  --man          # Open full program manual\n".
-	"  --quiet        # Run program with minimal output\n";
-	
-    if ($opt =~ "full") {
-	print "\n$usage\n\n";
-	print "$args\n\n";
+    my ($help_msg, $podfile) =  @_;
+    # help_msg is the type of help msg to use (ie. help vs. usage)
+    
+    print "\n";
+    
+    #-----------------------------+
+    # PIPE WITHIN PERL            |
+    #-----------------------------+
+    # This code made possible by:
+    # http://www.perlmonks.org/index.pl?node_id=76409
+    # Tie info developed on:
+    # http://www.perlmonks.org/index.pl?node=perltie 
+    #
+    #my $podfile = $0;
+    my $scalar = '';
+    tie *STDOUT, 'IO::Scalar', \$scalar;
+    
+    if ($help_msg =~ "usage") {
+	podselect({-sections => ["SYNOPSIS|MORE"]}, $0);
     }
     else {
-	print "\n$usage\n\n";
+	podselect({-sections => ["SYNOPSIS|ARGUMENTS|OPTIONS|MORE"]}, $0);
+    }
+
+    untie *STDOUT;
+    # now $scalar contains the pod from $podfile you can see this below
+    #print $scalar;
+
+    my $pipe = IO::Pipe->new()
+	or die "failed to create pipe: $!";
+    
+    my ($pid,$fd);
+
+    if ( $pid = fork() ) { #parent
+	open(TMPSTDIN, "<&STDIN")
+	    or die "failed to dup stdin to tmp: $!";
+	$pipe->reader();
+	$fd = $pipe->fileno;
+	open(STDIN, "<&=$fd")
+	    or die "failed to dup \$fd to STDIN: $!";
+	my $pod_txt = Pod::Text->new (sentence => 0, width => 78);
+	$pod_txt->parse_from_filehandle;
+	# END AT WORK HERE
+	open(STDIN, "<&TMPSTDIN")
+	    or die "failed to restore dup'ed stdin: $!";
+    }
+    else { #child
+	$pipe->writer();
+	$pipe->print($scalar);
+	$pipe->close();	
+	exit 0;
     }
     
-    exit;
-}
+    $pipe->close();
+    close TMPSTDIN;
 
+    print "\n";
+
+    exit 0;
+   
+}
 
 sub tenest2gff {
 # CONVERT BLAST TO GFF 
@@ -509,12 +459,12 @@ sub tenest2gff {
 
     # Open the BLAST report object
     open (TENESTIN,"<$tenestin")
-	|| die "Could not open TE-NEST input file:\n$tenestin.\n";
+	|| die "ERROR: Could not open TE-NEST input file:\n$tenestin.\n";
     
     # Open file handle to the gff outfile    
     if ($append) {
 	open (GFFOUT, ">>$gffout") 
-	    || die "Can not open file:\n $gffout\n";
+	    || die "ERROR: Can not open file:\n $gffout\n";
     }
     else {
 	open (GFFOUT, ">$gffout") 
@@ -994,14 +944,245 @@ sub tenest2gff {
     
 }
 
+1;
+__END__
+
+# Old print_help 
+sub print_help {
+
+    # Print requested help or exit.
+    # Options are to just print the full 
+    my ($opt) = @_;
+
+    my $usage = "USAGE:\n". 
+	"fetch_tenest.pl -i InFile -o OutFile";
+    my $args = "REQUIRED ARGUMENTS:\n".
+	"  --infile       # Path to the input file\n".
+	"  --outfile      # Path to the output file\n".
+	"\n".
+	"OPTIONS:\n".
+	"  --version      # Show the program version\n".     
+	"  --usage        # Show program usage\n".
+	"  --help         # Show this help message\n".
+	"  --man          # Open full program manual\n".
+	"  --quiet        # Run program with minimal output\n";
+	
+    if ($opt =~ "full") {
+	print "\n$usage\n\n";
+	print "$args\n\n";
+    }
+    else {
+	print "\n$usage\n\n";
+    }
+    
+    exit;
+}
+
+=head1 NAME
+
+fetch_tenest.pl - Fetch TE Nest LTR and SVG files
+
+=head1 VERSION
+
+This documentation refers to $Rev$
+
+=head1 SYNOPSIS
+
+=head2 Usage
+
+    fetch_tenest.pl -c SeqList.txt -o OutDir [--gff]
+
+=head2 Required Arguments
+
+    -c, --config   # Path to the config file
+    -o, --outdir   # Path to the base output directory
+    --gff          # Convert output to GFF files
+
+=head1 DESCRIPTION
+
+Given a tab delimited input file of theFetches the TE Nest results using wget.
+The results are converted to gff and copied to the gff dir if requested.
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -c,--config
+
+Path of the configuration file. This is a tab delimited input file that must be
+in the following format:
+
+    HEX0350E24     1188334554_18873
+    HEX0358K19     1188334578_19208
+    HEX0411K13     1188334604_19821
+
+Where the first column is the name of the sequence that was analyzed
+by TE Nest and the second column is the id of the TE Nest job. This job 
+id is returned by TE Nest when the job is initially submitted.
+
+=item -o,--outdir
+
+Path of the output directory. This is the base dir that the analysis 
+results are stored in. 
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item --gff
+
+Convert downloaded data to the GFF file format.
+
+=item --usage
+
+Short overview of how to use program from command line.
+
+=item --help
+
+Show program usage with summary of options.
+
+=item --version
+
+Show program version.
+
+=item --man
+
+Show the full program manual. This uses the perldoc command to print the 
+POD documentation for the program.
+
+=item -q,--quiet
+
+Run the program with minimal output.
+
+=back
+
+=head1 DIAGNOSTICS
+
+Error messages generated by this program and possible solutions are listed
+below.
+
+=over 2
+
+=item ERROR: Can not create tenest out dir.
+
+The output directory could not be created at the path you specified. 
+This could be do to the fact that the directory that you are trying
+to place your base directory in does not exist, or because you do not
+have write permission to the directory you want to place your file in.
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head2 Configuration File
+
+The configuration file used by fetch_tenest.pl a tab delimited 
+text file that must be in the following format:
+
+    HEX0350E24     1188334554_18873
+    HEX0358K19     1188334578_19208
+    HEX0411K13     1188334604_19821
+
+Where the first column is the name of the sequence that was analyzed
+by TE Nest and the second column is the id of the TE Nest job. This job 
+id is returned by TE Nest when the job is initially submitted.
+
+=head2 Environment
+
+The fetch_tenest.pl program does not currently make use of variables
+defined in the user's environment.
+
+=head1 DEPENDENCIES
+
+=head2 Required Software
+
+=over
+
+=item * TE Nest Web Server
+
+As of 12/12/2007 the only way to use the TE Nest program is through a web
+service provided by the Plant GDB web site 
+http://www.plantgdb.org/prj/TE_nest/TE_nest.html
+
+=back
+
+=head2 Required Perl Modules
+
+=over
+
+=item * File::Copy
+
+This module is required to copy the BLAST results.
+
+=item * Getopt::Long
+
+This module is required to accept options at the command line.
+
+=item * LWP::Simple
+
+This module is a simple interface to fetching files over the web.
+This is used to fetch results from the TE Nest web server.
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+=head2 Limitations
+
+=over
+
+=item * Config file must use UNIX format line endings
+
+The config file must have UNIX formatted line endings. Because of
+this any config files that have been edited in programs such as
+MS Word must be converted to a UNIX compatible text format before
+being used with batch_blast.
+
+=back
+
+=head1 SEE ALSO
+
+The fetch_tenest.pl program is part of the DAWG-PAWS package of genome
+annotation programs. See the DAWG-PAWS web page 
+( http://dawgpaws.sourceforge.net/ )
+or the Sourceforge project page 
+( http://sourceforge.net/projects/dawgpaws ) 
+for additional information about this package.
+
+=head1 LICENSE
+
+GNU GENERAL PUBLIC LICENSE, VERSION 3
+
+http://www.gnu.org/licenses/gpl.html   
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
 =head1 HISTORY
 
 STARTED: 08/29/2007
 
-UPDATED: 08/29/2007
+UPDATED: 12/12/2007
 
-VERSION: $Id: fetch_tenest.pl 85 2007-08-29 14:29:27Z JamesEstill $
+VERSION: $Rev$
 
 =cut
 
@@ -1012,3 +1193,9 @@ VERSION: $Id: fetch_tenest.pl 85 2007-08-29 14:29:27Z JamesEstill $
 # 08/29/2007
 # - Program started
 # - Added tenest2gff from cnv_tenest2gff.pl
+# 12/12/2007
+# - Moved POD documentation to the end of the script
+# - Added print_help subfunction to extract help and
+#   usage statements from the POD documentation
+# - Updated POD documentation
+# - Changed --infile to --config
