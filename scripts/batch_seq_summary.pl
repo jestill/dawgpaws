c98b0f8a611509db432b1c0dc41381b5c5db57b5
James Estill <JamesEstill@gmail.com>
2007-12-13 21:54:31 +0000

    Updated batch_seq_summary.pl # - Updated POD documentation # - Added print_help subfunction that extracts usage and #   help messages from the POD documentation # - Added check for required arguments

--- scripts/batch_seq_summary.pl
+++ scripts/batch_seq_summary.pl
@@ -8,10 +8,10 @@
 #  AUTHOR: James C. Estill                                  |
 # CONTACT: JamesEstill_@_gmail.com                          |
 # STARTED: 10/05/2007                                       |
-# UPDATED: 10/05/2007                                       |
+# UPDATED: 12/13/2007                                       |
 #                                                           |
 # DESCRIPTION:                                              |
-#  Display basic summary information for a direcotry of     |
+#  Display basic summary information for a directory of     |
 #  sequences. Files must end in fasta to be recognized      |
 #  as a fasta file.                                         |
 #                                                           |
@@ -33,6 +33,12 @@ package DAWGPAWS;
 #-----------------------------+
 use strict;
 use Getopt::Long;
+# The following needed for printing help
+use Pod::Select;               # Print subsections of POD documentation
+use Pod::Text;                 # Print POD doc as formatted text file
+use IO::Scalar;                # For print_help subfunction
+use IO::Pipe;                  # Pipe for STDIN, STDOUT for POD docs
+use File::Spec;                # Convert a relative path to an abosolute path
 
 #-----------------------------+
 # PROGRAM VARIABLES           |
@@ -79,13 +85,16 @@ my $ok = GetOptions(# REQUIRED OPTIONS
 #-----------------------------+
 # SHOW REQUESTED HELP         |
 #-----------------------------+
-if ($show_usage) {
-    print_help("");
+if ( ($show_usage) ) {
+#    print_help ("usage", File::Spec->rel2abs($0) );
+    print_help ("usage", $0 );
 }
 
-if ($show_help || (!$ok) ) {
-    print_help("full");
+if ( ($show_help) || (!$ok) ) {
+#    print_help ("help",  File::Spec->rel2abs($0) );
+    print_help ("help",  $0 );
 }
+
 
 if ($show_version) {
     print "\n$0:\nVersion: $VERSION\n\n";
@@ -97,6 +106,21 @@ if ($show_man) {
     system("perldoc $0");
     exit($ok ? 0 : 2);
 }
+
+
+#-----------------------------+
+# CHECK REQUIRED ARGS         |
+#-----------------------------+
+if ( (!$indir) || (!$outfile) ) {
+    print "\a";
+    print STDERR "\n";
+    print STDERR "ERROR: An input directory was not specified at the".
+	" command line\n" if (!$indir);
+    print STDERR "ERROR: An output file was not specified at the".
+	" command line\n" if (!$outfile);
+    print_help ("usage", $0 );
+}
+
 
 #-----------------------------------------------------------+
 # MAIN PROGRAM BODY                                         |
@@ -121,7 +145,7 @@ opendir( DIR, $indir ) ||
 my @fasta_files = grep /\.fasta$|\.fa$/, readdir DIR ;
 closedir( DIR );
 my $num_files = @fasta_files;
-sort @fasta_files;
+#sort @fasta_files;
 
 if ($num_files == 0) {
     print "\a";
@@ -161,6 +185,74 @@ exit;
 #-----------------------------------------------------------+
 
 sub print_help {
+    my ($help_msg, $podfile) =  @_;
+    # help_msg is the type of help msg to use (ie. help vs. usage)
+    
+    print "\n";
+    
+    #-----------------------------+
+    # PIPE WITHIN PERL            |
+    #-----------------------------+
+    # This code made possible by:
+    # http://www.perlmonks.org/index.pl?node_id=76409
+    # Tie info developed on:
+    # http://www.perlmonks.org/index.pl?node=perltie 
+    #
+    #my $podfile = $0;
+    my $scalar = '';
+    tie *STDOUT, 'IO::Scalar', \$scalar;
+    
+    if ($help_msg =~ "usage") {
+	podselect({-sections => ["SYNOPSIS|MORE"]}, $0);
+    }
+    else {
+	podselect({-sections => ["SYNOPSIS|ARGUMENTS|OPTIONS|MORE"]}, $0);
+    }
+
+    untie *STDOUT;
+    # now $scalar contains the pod from $podfile you can see this below
+    #print $scalar;
+
+    my $pipe = IO::Pipe->new()
+	or die "failed to create pipe: $!";
+    
+    my ($pid,$fd);
+
+    if ( $pid = fork() ) { #parent
+	open(TMPSTDIN, "<&STDIN")
+	    or die "failed to dup stdin to tmp: $!";
+	$pipe->reader();
+	$fd = $pipe->fileno;
+	open(STDIN, "<&=$fd")
+	    or die "failed to dup \$fd to STDIN: $!";
+	my $pod_txt = Pod::Text->new (sentence => 0, width => 78);
+	$pod_txt->parse_from_filehandle;
+	# END AT WORK HERE
+	open(STDIN, "<&TMPSTDIN")
+	    or die "failed to restore dup'ed stdin: $!";
+    }
+    else { #child
+	$pipe->writer();
+	$pipe->print($scalar);
+	$pipe->close();	
+	exit 0;
+    }
+    
+    $pipe->close();
+    close TMPSTDIN;
+
+    print "\n";
+
+    exit 0;
+   
+}
+
+
+1;
+__END__
+
+# Deprecated print_help
+sub print_help {
 
     # Print requested help or exit.
     # Options are to just print the full 
@@ -197,39 +289,41 @@ batch_seq_sumary.pl - Print summary info for a directory of sequence files
 
 =head1 VERSION
 
-This documentation refers to program version 0.1
+This documentation refers to program version $Rev$
 
 =head1 SYNOPSIS
 
-  USAGE:
-    Name.pl -i InFile -o OutFile
+=head2 Usage
 
-    --infile        # Path to the input file
-    --outfie        # Path to the output file
+    batch_seq_summary.pl -i InFile -o OutFile
+
+=head2 Required Arguments
+
+    -i, --indir    # Directory of fasta files to process
+    -o, --outfile  # Path to file to print program output
 
 =head1 DESCRIPTION
 
-This is what the program does
+Display basic summary information for a directory of
+sequences. Files must end in fasta to be recognized
+as a fasta file.
 
-=head1 COMMAND LINE ARGUMENTS
-
-=head 2 Required Arguments
+=head1 REQUIRED ARGUMENTS
 
 =over 2
 
-=item -i,--infile
+=item -i,--indir
 
-Path of the input file.
+Path of the directory containing the sequences to process.
 
 =item -o,--outfile
 
-Path of the output file.
+Path of the output file that will contain information for the
+input sequence files.
 
 =back
 
-=head1 Additional Options
-
-=over
+=head1 OPTIONS
 
 =over 2
 
@@ -254,35 +348,102 @@ POD documentation for the program.
 
 Run the program with minimal output.
 
+=item --verbose
+
+Run the program in verbose mode.
+
 =back
 
 =head1 DIAGNOSTICS
 
-The list of error messages that can be generated,
-explanation of the problem
-one or more causes
-suggested remedies
-list exit status associated with each error
+Error messages generated by this program and possible solutions are listed
+below.
+
+=over 2
+
+=item ERROR: No fasta files were found in the input directory
+
+The input directory does not contain fasta files in the expected format.
+This could happen because you gave an incorrect path or because your sequence 
+files do not have the expected *.fasta extension in the file name.
+
+=item ERROR: Could not create the output file
+
+The output file path may be to a directory that does not exist
+or you may not have write permission to the directory that you
+are trying to write to.
+
+=back
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-Names and locations of config files
-environmental variables
-or properties that can be set.
+the batch_seq_summary.pl program does not require an external configuration
+file or make use of variables set in the user's environment.
 
 =head1 DEPENDENCIES
 
-Other modules or software that the program is dependent on.
+=head2 Required Software
+
+=over 2
+
+=item * seq_countbase_non.sh
+
+This shell script is included in the DAWG-PAWS package. This will
+count the non N bases in an input sequence file.
+
+=back
+
+=head2 Required Perl Modules
+
+=over
+
+=item * Getopt::Long
+
+This module is required to accept options at the command line.
+
+=back
 
 =head1 BUGS AND LIMITATIONS
 
-Any known bugs and limitations will be listed here.
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+=head2 Limitations
+
+=over
+
+=item * Limited to Linux/Unix environments
+
+This program requires the use of a shell script that will only
+work in a Linux or Unix environment.
+
+=back
+
+=head1 SEE ALSO
+
+The batch_seq_summary.pl program is part of the DAWG-PAWS package of genome
+annotation programs. See the DAWG-PAWS web page 
+( http://dawgpaws.sourceforge.net/ )
+or the Sourceforge project page 
+( http://sourceforge.net/projects/dawgpaws ) 
+for additional information about this package.
 
 =head1 LICENSE
 
-GNU LESSER GENERAL PUBLIC LICENSE
+GNU GENERAL PUBLIC LICENSE, VERSION 3
 
-http://www.gnu.org/licenses/lgpl.html
+http://www.gnu.org/licenses/gpl.html
+
+THIS SOFTWARE COMES AS IS, WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTY. USE AT YOUR OWN RISK.
 
 =head1 AUTHOR
 
@@ -290,9 +451,9 @@ James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
 =head1 HISTORY
 
-STARTED:
+STARTED: 10/05/2007
 
-UPDATED:
+UPDATED: 12/13/2007
 
 VERSION: $Rev$
 
@@ -302,3 +463,13 @@ VERSION: $Rev$
 # HISTORY                                                   |
 #-----------------------------------------------------------+
 #
+# 10/05/07
+# - Main program body written
+# - Uses shell script to fetch non N characters for 
+#   each of the sequence files
+# 
+# 12/13/2007
+# - Updated POD documentation
+# - Added print_help subfunction that extracts usage and
+#   help messages from the POD documentation
+# - Added check for required arguments
