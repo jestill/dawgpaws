cc0afdb145764c3001d252369809df76a64f2d97
James Estill <JamesEstill@gmail.com>
2007-12-13 17:42:51 +0000

    updated batch_ltrseq.pl # Updated POD documentation # Currently not a functional program

--- scripts/batch_ltrseq.pl
+++ scripts/batch_ltrseq.pl
@@ -17,6 +17,108 @@
 #                                                           |
 #-----------------------------------------------------------+
 
+package DAWGPAWS;
+
+#-----------------------------+
+# INCLUDES                    |
+#-----------------------------+
+use strict;
+use Getopt::Long;
+
+#-----------------------------+
+# PROGRAM VARIABLES           |
+#-----------------------------+
+my ($VERSION) = q$Rev$ =~ /(\d+)/;
+
+#-----------------------------+
+# VARIABLE SCOPE              |
+#-----------------------------+
+my $infile;
+my $outfile;
+
+# Booleans
+my $help = 0;
+my $quiet = 0;
+my $show_help = 0;
+my $show_usage = 0;
+my $show_man = 0;
+my $show_version = 0;
+
+#-----------------------------+
+# COMMAND LINE OPTIONS        |
+#-----------------------------+
+my $ok = GetOptions("i|infile=s"  => \$infile,
+                    "o|outfile=s" => \$outfile,
+		    "usage"       => \$show_usage,
+		    "version"     => \$show_version,
+		    "man"         => \$show_man,
+		    "h|help"      => \$show_help,
+		    "q|quiet"     => \$quiet,);
+
+#-----------------------------+
+# SHOW REQUESTED HELP         |
+#-----------------------------+
+if ($show_usage) {
+    print_help("");
+}
+
+if ($show_help || (!$ok) ) {
+    print_help("full");
+}
+
+if ($show_version) {
+    print "\n$0:\nVersion: $VERSION\n\n";
+    exit;
+}
+
+if ($show_man) {
+    # User perldoc to generate the man documentation.
+    system("perldoc $0");
+    exit($ok ? 0 : 2);
+}
+
+#-----------------------------+
+# MAIN PROGRAM BODY           |
+#-----------------------------+
+
+# PROGRAM IN DEVELOPMENT
+
+exit;
+
+#-----------------------------------------------------------+ 
+# SUBFUNCTIONS                                              |
+#-----------------------------------------------------------+
+
+sub print_help {
+
+    # Print requested help or exit.
+    # Options are to just print the full 
+    my ($opt) = @_;
+
+    my $usage = "USAGE:\n". 
+	"MyProg.pl -i InFile -o OutFile";
+    my $args = "REQUIRED ARGUMENTS:\n".
+	"  --infile       # Path to the input file\n".
+	"  --outfile      # Path to the output file\n".
+	"\n".
+	"OPTIONS::\n".
+	"  --version      # Show the program version\n".     
+	"  --usage        # Show program usage\n".
+	"  --help         # Show this help message\n".
+	"  --man          # Open full program manual\n".
+	"  --quiet        # Run program with minimal output\n";
+	
+    if ($opt =~ "full") {
+	print "\n$usage\n\n";
+	print "$args\n\n";
+    }
+    else {
+	print "\n$usage\n\n";
+    }
+    
+    exit;
+}
+
 =head1 NAME
 
 batch_ltrseq.pl - Run the LTR_seq program in batch mode.
@@ -91,133 +193,80 @@ or properties that can be set.
 
 =head1 DEPENDENCIES
 
-Other modules or software that the program is dependent on.
+=head2 Required Software
+
+=over
+
+=item * ltr_seq
+
+The ltr_seq is the latest incarnation of the ltr_par program
+described by Kalyanaraman and Aluru J. Bioinform Comput Biol 4: 197-216.
+A binary of this program is available upon contacuting the program
+author: ananth <at> eecs.wsu.edu. The author's current homepage is:
+http://www.eecs.wsu.edu/~ananth/
+
+=back
+
+=head2 Required Perl Modules
+
+=over
+
+=item * File::Copy
+
+This module is required to copy the BLAST results.
+
+=item * Getopt::Long
+
+This module is required to accept options at the command line.
+
+=back
 
 =head1 BUGS AND LIMITATIONS
 
-Any known bugs and limitations will be listed here.
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+=head2 Limitations
+
+=over
+
+=item * Config file must use UNIX format line endings
+
+The config file must have UNIX formatted line endings. Because of
+this any config files that have been edited in programs such as
+MS Word must be converted to a UNIX compatible text format before
+being used with batch_blast.
+
+=back
 
 =head1 LICENSE
 
-GNU LESSER GENERAL PUBLIC LICENSE
+GNU GENERAL PUBLIC LICENSE, VERSION 3
 
-http://www.gnu.org/licenses/lgpl.html
+http://www.gnu.org/licenses/gpl.html
+
+THIS SOFTWARE COMES AS IS, WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTY. USE AT YOUR OWN RISK.
 
 =head1 AUTHOR
 
 James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
-=cut
-
-package DAWGPAWS;
-
-#-----------------------------+
-# INCLUDES                    |
-#-----------------------------+
-use strict;
-use Getopt::Long;
-
-#-----------------------------+
-# PROGRAM VARIABLES           |
-#-----------------------------+
-my ($VERSION) = q$Rev$ =~ /(\d+)/;
-
-#-----------------------------+
-# VARIABLE SCOPE              |
-#-----------------------------+
-my $infile;
-my $outfile;
-
-# Booleans
-my $help = 0;
-my $quiet = 0;
-my $show_help = 0;
-my $show_usage = 0;
-my $show_man = 0;
-my $show_version = 0;
-
-#-----------------------------+
-# COMMAND LINE OPTIONS        |
-#-----------------------------+
-my $ok = GetOptions("i|infile=s"  => \$infile,
-                    "o|outfile=s" => \$outfile,
-		    "usage"       => \$show_usage,
-		    "version"     => \$show_version,
-		    "man"         => \$show_man,
-		    "h|help"      => \$show_help,
-		    "q|quiet"     => \$quiet,);
-
-
-#-----------------------------+
-# SHOW REQUESTED HELP         |
-#-----------------------------+
-if ($show_usage) {
-    print_help("");
-}
-
-if ($show_help || (!$ok) ) {
-    print_help("full");
-}
-
-if ($show_version) {
-    print "\n$0:\nVersion: $VERSION\n\n";
-    exit;
-}
-
-if ($show_man) {
-    # User perldoc to generate the man documentation.
-    system("perldoc $0");
-    exit($ok ? 0 : 2);
-}
-
-#-----------------------------+
-# MAIN PROGRAM BODY           |
-#-----------------------------+
-
-exit;
-
-#-----------------------------------------------------------+ 
-# SUBFUNCTIONS                                              |
-#-----------------------------------------------------------+
-
-sub print_help {
-
-    # Print requested help or exit.
-    # Options are to just print the full 
-    my ($opt) = @_;
-
-    my $usage = "USAGE:\n". 
-	"MyProg.pl -i InFile -o OutFile";
-    my $args = "REQUIRED ARGUMENTS:\n".
-	"  --infile       # Path to the input file\n".
-	"  --outfile      # Path to the output file\n".
-	"\n".
-	"OPTIONS::\n".
-	"  --version      # Show the program version\n".     
-	"  --usage        # Show program usage\n".
-	"  --help         # Show this help message\n".
-	"  --man          # Open full program manual\n".
-	"  --quiet        # Run program with minimal output\n";
-	
-    if ($opt =~ "full") {
-	print "\n$usage\n\n";
-	print "$args\n\n";
-    }
-    else {
-	print "\n$usage\n\n";
-    }
-    
-    exit;
-}
-
-
 =head1 HISTORY
 
-STARTED:
+STARTED: 09/06/2007
 
-UPDATED:
+UPDATED: 12/13/2007
 
-VERSION: $Id: script_template.pl 68 2007-07-15 00:25:18Z JamesEstill $
+VERSION: $Rev$
 
 =cut
 
@@ -225,3 +274,11 @@ VERSION: $Id: script_template.pl 68 2007-07-15 00:25:18Z JamesEstill $
 # HISTORY                                                   |
 #-----------------------------------------------------------+
 #
+# 09/06/2007
+# - Program started, bulk of program written
+# 
+# 12/13/2007
+# - Moved POD documentation to the end of the program
+# - Added new print_help subfunction that extracts usage
+#   and help statements from POD documentation
+# - Updated POD documentation
