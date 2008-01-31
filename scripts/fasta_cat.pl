0e48d74f23546e44660c53192db0abfa76eaa664
James Estill <JamesEstill@gmail.com>
2008-01-31 04:33:20 +0000

    Updated fasta_cat.pl # Modified to fit in better with the rest of the DAWG-PAWS code

--- scripts/fasta_cat.pl
+++ scripts/fasta_cat.pl
@@ -1,6 +1,112 @@
 #!/usr/bin/perl -w
-# STARTED: 08/20/2006
-# UPDATED: 04/30/2007
+#-----------------------------------------------------------+
+#                                                           |
+# fasta_cat.pl - Concatenate all fasta files in a directroy |
+#                                                           |
+#-----------------------------------------------------------+
+#                                                           |
+# STARTED: 08/20/2006                                       |
+# UPDATED: 01/30/2008                                       |
+#                                                           |
+#-----------------------------------------------------------+
+# TODO:
+# Update to Getopt::Long
+use Getopt::Long;
+#use Getopt::Std;
+
+#
+my $indir;
+my $outfile;
+
+# BOOLEANS                                                                                  
+my $show_help = 0;             # Show program help                                          
+my $show_version = 0;          # Show program version                                       
+my $show_man = 0;              # Show program manual page using peldoc                      
+my $show_usage = 0;            # Show program usage command                                 
+my $quiet = 0;                 # Boolean for reduced output to STOUT                        
+my $test = 0;                  # Run the program in test mode                               
+my $verbose = 0;               # Run the program in verbose mode 
+
+#-----------------------------+
+# GET OPTIONS FROM THE        |
+# COMMAND LINE                |
+#-----------------------------+
+my $ok = GetOptions(
+                    # Required Arguments                                                    
+                    "i|indir=s"    => \$indir,
+                    "o|outfile=s"  => \$outfile,
+                    # Booleans                                                              
+                    "verbose"      => \$verbose,
+                    "test"         => \$test,
+                    "usage"        => \$show_usage,
+                    "version"      => \$show_version,
+                    "man"          => \$show_man,
+                    "h|help"       => \$show_help,
+                    "q|quiet"      => \$quiet,);
+
+#-----------------------------+                                                             
+# CHECK FOR SLASH IN DIR      |                                                             
+# VARIABLES                   |                                                             
+#-----------------------------+ 
+unless ($outdir =~ /\/$/ ) {
+    $outdir = $outdir."/";
+}
+
+&cat_fasta ($indir, $outfile);
+
+exit;
+
+sub cat_fasta { #  Begin of CatBlast Subfunction
+    
+    my $indir = $_[0];
+    my $fasta_file_path = $_[1];
+    
+    my $file_num = 0;
+    
+    # First warn the user if the output file already exists
+    my $ArrayRefI = 0;                # Start the arrray ref at 0
+    
+    # Set the current working directory to the directory that was selected
+    #chdir $Directory;
+    
+    #-----------------------------+                                                             
+    # Get the FASTA files from the|                                                             
+    # directory provided by the   |                                                             
+    # var $indir                  |                                                             
+    #-----------------------------+                                                             
+    opendir( DIR, $indir ) ||
+	die "Can't open directory:\n$indir";
+    my @fasta_files = grep /\.fasta$|\.fa$/, readdir DIR ;
+    closedir( DIR );
+    
+    my $total_files = @fasta_files;
+    
+    # Currently this is set to append to an existing file
+    #my $NumFiles = @FastaList;
+    open(FASTAOUT, ">> $fasta_file_path") ||
+	die "Can not open fasta file:\n$fasta_file_path\n";
+    
+    for my $ind_file (@fasta_files) {
+	
+	$file_num++;
+	my $in_file_path = $indir.$ind_file;
+
+	print "Processing ".$file_num." of ".$total_files."\n"; 
+	open(FASTAFILE, $in_file_path) || 
+	    die "Can not open input file:\n$in_file_path";
+	
+	while (<FASTAFILE>) {  # For every line in the FASTA file
+	    print FASTAOUT ;   # Print the output to FASTAOUT
+	}
+	close(FASTAFILE);
+	
+    } # End of foreach @BlastList loop
+    
+    close(FASTAOUT);
+    
+} # End of cat_fasta Subfunction
+
+__END__
 
 =head1 NAME
 
@@ -35,66 +141,6 @@ James C. Estill<lt>JamesEstill at gmail.comE<gt>
 
 =cut
 
-use Getopt::Std;
-
-#-----------------------------+
-# GET OPTIONS FROM THE        |
-# COMMAND LINE                |
-#-----------------------------+
-getopts('i:o:', \%Options);
-
-my $InDir = $Options{i};
-my $OutFile = $Options{o};
-
-&CatFasta ($InDir, $OutFile);
-exit;
-
-sub CatFasta
-    
-{ #  Begin of CatBlast Subfunction
-
-
-    my $FileNum = 0;
-    my $Directory = $_[0];
-    my $FastaFileOut = $_[1];
-    # First warn the user if the output file already exists
-    my $ArrayRefI = 0;                # Start the arrray ref at 0
-    
-    # Set the current working directory to the directory that was selected
-    chdir $Directory;
-    
-    # -------------------------------------------
-    # Load an array with the files to concateate
-    # -------------------------------------------
-    # Open the directory containing the BLAST output files
-    opendir( DIR, $Directory ) ||
-	die "Can not open directory\n$Directory\n";
-    # Read the directory ignoring the . and ..
-    my @FastaList = grep !/^\.\.?$/, readdir DIR ;
-    closedir( DIR );
-    
-    my $NumFiles = @FastaList;
-    open(FASTAOUT, ">> $FastaFileOut");
-    
-  FILE: foreach (@FastaList)
-  { # Begin of foreach loop
-      # Temp exit for debug
-      #if ($FileNum == 50) { exit; } 
-      # Show the name of the file that is being added
-      
-      # Show the number of the file being processed
-      print "Processing ".$FileNum." of ".$NumFiles."\n"; 
-      open(FASTAFILE, $_) || ((warn "Can't open file $_\n"), next FILE);
-      
-      while (<FASTAFILE>) {  # For every line in the FASTA file
-	  print FASTAOUT ;   # Print the output to FASTAOUT
-      }
-      close(FASTAFILE);
-  } # End of foreach @BlastList loop
-    
-    close(FASTAOUT);
-    
-} # End of CatFasta Subfunction
 
 #-----------------------------------------------------------+
 # HISTORY                                                   |
