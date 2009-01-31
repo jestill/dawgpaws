951a30c52acd5970ffd1752f9c6159a0ac279d25
James Estill <JamesEstill@gmail.com>
2009-01-31 04:28:47 +0000

    Updated cnv_blast2gff.pl  -Updated POD documentation  -Moved POD documentation to end of the program  -Updated to new print_help subfunction that extracts   help from POD  -Added default to print to STDOUT if not outfile   path given at command line  -Added default input from STDIN in no infile path   given at command line  -Added package name DAWGPAWS  -Added options to pass database name, query sequence id,   and program used. Otherwise these are attempted to   be extracted from the command line  -Added Rev to SVN propset for this program

--- scripts/cnv_blast2gff.pl
+++ scripts/cnv_blast2gff.pl
@@ -1,18 +1,22 @@
 #!/usr/bin/perl -w
 #-----------------------------------------------------------+
 #                                                           |
-# cnv_blast2gff.pl Convert BLAST output to gff 
+# cnv_blast2gff.pl - Convert BLAST output to gff            |
 #                                                           |
 #-----------------------------------------------------------+
 #  AUTHOR: James C. Estill                                  |
 # CONTACT: JamesEstill_at_gmail.com                         |
 # STARTED: 07/06/2006                                       |
-# UPDATED: 08/06/2007                                       |
+# UPDATED: 01/30/2009                                       |
 #                                                           |  
 # DESCRIPTION:                                              | 
-# Convert blast output to a Apollo compatible gff file.     |
+#  Convert blast output to a Apollo compatible gff file.    |
 #                                                           |
 # USAGE:                                                    |
+#  cnv_blast2gff.pl -i infile.bln -o blast_out.gff          |
+#                                                           |
+# VERSION:                                                  |
+#  $Rev$                                                    |
 #                                                           |
 # LICENSE:                                                  |
 #  GNU General Public License, Version 3                    |
@@ -20,97 +24,7 @@
 #                                                           |
 #-----------------------------------------------------------+
 
-=head1 NAME
-
-Name.pl - Short program description. 
-
-=head1 VERSION
-
-This documentation refers to program version 0.1
-
-=head1 SYNOPSIS
-
- Usage:
-  Name.pl -i InFile -o OutFile
-
-=head1 DESCRIPTION
-
-This is what the program does
-
-=head1 REQUIRED ARGUMENTS
-
-=over 2
-
-=item -i,--infile
-
-Path of the input file.
-
-=item -o,--outfile
-
-Path of the output file.
-
-=back
-
-=head1 OPTIONS
-
-=over 2
-
-=item --usage
-
-Short overview of how to use program from command line.
-
-=item --help
-
-Show program usage with summary of options.
-
-=item --version
-
-Show program version.
-
-=item --man
-
-Show the full program manual. This uses the perldoc command to print the 
-POD documentation for the program.
-
-=item -q,--quiet
-
-Run the program with minimal output.
-
-=back
-
-=head1 DIAGNOSTICS
-
-The list of error messages that can be generated,
-explanation of the problem
-one or more causes
-suggested remedies
-list exit status associated with each error
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Names and locations of config files
-environmental variables
-or properties that can be set.
-
-=head1 DEPENDENCIES
-
-Other modules or software that the program is dependent on.
-
-=head1 BUGS AND LIMITATIONS
-
-Any known bugs and limitations will be listed here.
-
-=head1 LICENSE
-
-GNU LESSER GENERAL PUBLIC LICENSE
-
-http://www.gnu.org/licenses/lgpl.html
-
-=head1 AUTHOR
-
-James C. Estill E<lt>JamesEstill at gmail.comE<gt>
-
-=cut
+package DAWGPAWS;
 
 #-----------------------------+
 # INCLUDES                    |
@@ -118,26 +32,32 @@ James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 use strict;
 use Getopt::Long;
 use Bio::SearchIO;             # Parse BLAST output
+# The following needed for printing help
+use Pod::Select;               # Print subsections of POD documentation
+use Pod::Text;                 # Print POD doc as formatted text file
+use IO::Scalar;                # For print_help subfunction
+use IO::Pipe;                  # Pipe for STDIN, STDOUT for POD docs
+use File::Spec;                # Convert a relative path to an abosolute path
 
 #-----------------------------+
 # PROGRAM VARIABLES           |
 #-----------------------------+
-my $VERSION = "0.1";
+my ($VERSION) = q$Rev:$ =~ /(\d+)/;
 
 #-----------------------------+
 # LOCAL VARIABLES             |
 #-----------------------------+
-my $BlastFile = "/home/jestill/projects/asgr/Asgr_c_Net_BigCat/CPAN001/".
-    "contig02/contig02_CPAN001.blo";
-my $GFFOut = "/home/jestill/projects/asgr/Asgr_c_Net_BigCat/CPAN001/".
-    "contig02/contig02_CPAN001.gff";
-my $max_e = 0.00001;
-my $min_len = 100;
+my $max_e;
+my $min_len;
 
 # Set variable scope
-my $seqname;
-my $infile;
-my $outfile;
+#my $seqname = "seq";
+my $qry_name;
+my $infile;                     # Default to STDIN if not given
+my $outfile;                    # Default to STDOUT if not given
+my $blast_program = "blast";
+my $blast_alignment = 0;        # Alternatives include 8 and 9
+my $param;
 
 # Booleans
 my $verbose = 0;
@@ -149,24 +69,29 @@ my $show_man = 0;
 my $show_version = 0;
 my $do_append = 0;
 
+
 #-----------------------------+
 # COMMAND LINE OPTIONS        |
 #-----------------------------+
 my $ok = GetOptions(# REQUIRED
-		    "i|infile=s"  => \$infile,
-                    "o|outfile=s" => \$outfile,
-		    "n|name=s"    => \$seqname,
+		    "i|infile=s"   => \$infile,
+                    "o|outfile=s"  => \$outfile,
+		    "n|name=s"     => \$qry_name,
 		    # OPTIONS
-		    "verbose"     => \$verbose,
-		    "append"      => \$do_append,
-		    "maxe"        => \$max_e,
-		    "minlen"      => \$min_len,
-		    "usage"       => \$show_usage,
-		    "version"     => \$show_version,
-		    "man"         => \$show_man,
-		    "h|help"      => \$show_help,
-		    "q|quiet"     => \$quiet,);
-
+		    "p|program=s"  => \$blast_program,
+		    "m|align=s"    => \$blast_alignment,
+		    "d|database=s" => \$param,
+		    "e|maxe=s"     => \$max_e,
+		    "l|minlen=s"   => \$min_len,
+		    # Booleans
+		    "verbose"      => \$verbose,
+		    "append"       => \$do_append,
+		    "usage"        => \$show_usage,
+		    "version"      => \$show_version,
+		    "man"          => \$show_man,
+		    "h|help"       => \$show_help,
+		    "q|quiet"      => \$quiet,);
+ 
 #-----------------------------+
 # SHOW REQUESTED HELP         |
 #-----------------------------+
@@ -189,20 +114,13 @@ if ($show_man) {
     exit($ok ? 0 : 2);
 }
 
-#-----------------------------+
-# CHECK REQUIRED OPTIONS      |
-#-----------------------------+
-if ( (!$infile) || (!$outfile) || (!$seqname) ) {
-    print_help("full");
-}
-
 #-----------------------------------------------------------+
 # MAIN PROGRAM BODY                                         |
 #-----------------------------------------------------------+
-blast2gff ($infile, $outfile, $do_append, $seqname);
+blast2gff ($infile, $outfile, $do_append, $qry_name, $blast_alignment,
+	   $param, $blast_program);
 
 exit;
-
 
 
 #-----------------------------------------------------------+
@@ -216,7 +134,11 @@ sub blast2gff {
     # blastin - path to the blast input file
     # gffout  - path to the gff output file
     # append  - boolean append data to existing file at gff out
-    my ($blastin, $gffout, $append, $seqname) = @_;
+    # align   - the alignment type of the blast output
+    # suffix  - the suffix to add to type of blast
+    # prog    - blast program (blastn, blastx, wublast etc)
+    # gff second column is built as progparam
+    my ($blastin, $gffout, $append, $seqname, $align, $suffix, $prog) = @_;
     my $blastprog;        # Name of the blast program used (blastn, blastx)
     my $dbname;           # Name of the database blasted
     my $hitname;          # Name of the hit
@@ -224,27 +146,79 @@ sub blast2gff {
     my $end;              # End of the feature
     my $strand;           # Strand of the hit
 
-    # Open the BLAST report object
-    my $blast_report = new Bio::SearchIO ( '-format' => 'blast',
-					   '-file'   => $blastin,
-					   '-signif' => $max_e,
-					   '-min_query_len' => $min_len) 
-	|| die "Could not open BLAST input file:\n$blastin.\n";
+    # Recode from number for tab delimited blast
+    if ($align == 8 || $align ==9) {
+	$align = "tab";
+    }
     
-    # Open file handle to the gff outfile    
-    if ($append) {
-	open (GFFOUT, ">>$gffout") 
-	    || die "Can not open file:\n $GFFOut\n";
+    #-----------------------------+
+    # BLAST INTPUT OBJECT         |
+    #-----------------------------+
+    # This allows for input from NCBI-BLAST, and WUBLAST in either 
+    # default or tab delimited output as well as 
+    # INPUT FROM FILE PATH
+    my $blast_report;
+    if ($blastin) {
+	
+	# TAB ALIGNED BLAST OUTPUT
+	if ($align =~ "tab" ) {
+	    $blast_report = new Bio::SearchIO::blasttable ( 
+		'-format' => 'blasttable',
+		'-file'   => $blastin )
+		|| die "Could not open BLAST input file:\n$blastin.\n";
+	}
+	else {
+	    $blast_report = new Bio::SearchIO ( '-format' => 'blast',
+						'-file'   => $blastin,
+						'-signif' => $max_e,
+						'-min_query_len' => $min_len) 
+		|| die "Could not open BLAST input file:\n$blastin.\n";
+	} # Default to default blast alignment
+    }
+
+    # INPUT FROM STDIN
+    else {
+	if ($align =~ "tab") {
+	    $blast_report = new Bio::SearchIO::blasttable ( 
+		'-format' => 'blasttable',
+		'-fh'     => \*STDIN )
+		|| die "Could not open STDIN for blast table input\n";
+	}
+	else {
+	    print STDERR "Expecting input from STDIN\n";
+	    $blast_report = new Bio::SearchIO ( '-format' => 'blast',
+						'-fh'     => \*STDIN,
+						'-signif' => $max_e,
+						'-min_query_len' => $min_len) 
+		|| die "Could not open STDIN for blast input\n";
+	}
+    }
+    
+    #-----------------------------+
+    # FILEHANDLE FOR OUTPUT       |
+    #-----------------------------+
+    if ($gffout) {
+	if ($append) {
+	    open (GFFOUT, ">>$gffout") 
+		|| die "Can not open file:\n $gffout\n";
+	}
+	else {
+	    open (GFFOUT, ">$gffout") 
+	    || die "Can not open file:\n $gffout\n";
+	}
     }
     else {
-	open (GFFOUT, ">$gffout") 
-	    || die "Can not open file:\n $GFFOut\n";
+	open (GFFOUT, ">&STDOUT") ||
+	    die "Can not print to STDOUT\n";
     }
     
     while (my $blast_result = $blast_report->next_result())
     {
+
+	# Not using the following for the moment
+	# 01/30/2009
 	$blastprog = $blast_result->algorithm;
-	$dbname = $blast_result->database_name();
+	#$dbname = $blast_result->database_name();
 
     	while (my $blast_hit = $blast_result->next_hit())
 	{
@@ -263,51 +237,74 @@ sub blast2gff {
 		    $strand = "+";
 		}
 		else {
-		    die "Error parsing strand\n";
+		    $strand = "+";
 		}
 		
+		#-----------------------------+
+		# SET START AND END           |
+		#-----------------------------+
 		# Make certain that start coordinate is
 		# less then the end coordinate
 		if ( $blast_hsp->start() < $blast_hsp->end() ) {
-		    $start = $blast_hsp->start();         # Start
+		    $start = $blast_hsp->start();
 		    $end = $blast_hsp->end();
-		    #$strand = "+";
-		    
 		}
 		else {
 		    #
-		    $start = $blast_hsp->end();         # Start
+		    $start = $blast_hsp->end();
 		    $end = $blast_hsp->start();
-		    #$strand = "-";
+		}
+
+		#-----------------------------+
+		# GET ALGORITHM IF UNKNOWN    |
+		#-----------------------------+
+		# This attempts to get the algorithm from the
+		# blast report, otherwise the generic 'blast' is used
+		unless ($prog) {
+		    if ($blast_hit->algorithm) {
+			$prog = $blast_hit->algorithm;
+		    }
+		    else {
+			$prog = "blast";
+		    }
+		}
+
+		#-----------------------------+
+		# APPEND DB IDENTIFIER        |
+		#-----------------------------+
+		# Providing a suffix at the command line will override
+		# the name provided in the blast report
+		my $source = $prog;
+		if ($suffix) {
+		    $source = $source.":".$suffix;
+		}
+		elsif ($blast_result->database_name()) {
+		    $source = $source.":".$blast_result->database_name();
 		}
 
 
 		#-----------------------------+
-		# PRINT OUTPUT TO TERMINAL    |
+		# GET SEQNAME IF NOT PROVIDED |
 		#-----------------------------+
-		if ($verbose) {
-		    print
-			"$seqname\t".                            # Seqname
-			"$blastprog:$dbname\t".                  # Source
-			"exon\t".                                # Feature type name
-			"$start\t".                              # Start
-			"$end\t".                                # End
-			$blast_hsp->score()."\t".                # Score
-			"$strand\t".
-			".\t".                                   
-			"$hitname\n";                            # Feature name
-		}		
+		# First attempt to extract from blast report
+		# otherwise use 'seq' as the indentifier.
+		# For multiple query sequence, this will use
+		# the same identifier for all
+		unless ($seqname) {
+		    if ($blast_result->query_name) {
+			$seqname = $blast_result->query_name();
+		    }
+		    else {
+			$seqname = "seq";
+		    }
+		}
 		
 		#-----------------------------+
 		# PRINT OUTPUT TO GFF FILE    |
 		# WITH BAC DATA               |
 		#-----------------------------+
-		# Changing BLASTN to the Bac Name appears to allow 
-		# these to be drawn on different levels.
-		print GFFOUT 
-		    #$blast_hit->name()."\t".
-		    "$seqname\t".                            # Seqname
-		    "$blastprog:$dbname\t".                  # Source
+		print GFFOUT "$seqname\t".                   # Seqname
+		    "$source\t".                             # Source
 		    "exon\t".                                # Feature type name
 		    "$start\t".                              # Start
 		    "$end\t".                                # End
@@ -355,14 +352,226 @@ sub print_help {
     exit;
 }
 
+=head1 NAME
+
+cnv_blast2gff.pl - Convert blast output to GFF
+
+=head1 VERSION
+
+This documentation refers to program version $Rev$
+
+=head1 SYNOPSIS
+
+=head2 Usage
+
+    cnv_blast2gff.pl -i blast_report.bln -o blast_out.gff -n seq_id
+
+=head2 Required Arguments
+
+    -i      # Path to the input file
+            # If not specified the program will expect input from STDIN
+    -o      # Path to the output file
+            # If not specified the program will write to STDOUT
+    -n      # Name of the query sequence
+            # If a name is not specified, the name will be
+            # extracted from the blast report or use "seq"
+
+=head1 DESCRIPTION
+
+This program will translate a blast report for a single query sequence into
+the GFF format. Since this uses the general bioperl BLAST parser code,
+this should also be able to parse output from BLAT or wublast. This code
+works best for converting a blast report of a single query sequence against
+a single database.
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -i,--infile
+
+Path of the input file. If an input file is not specified, the program
+will expect input from STDIN.
+
+=item -o,--outfile
+
+Path of the output file. If an output file is not specified, the program
+will write output to STDOUT.
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item -n,--name
+
+Identifier of the sequence that has been used as the query sequence
+in the blast report. This will be used as the first column in the 
+gff output file.
+
+=item -p,--program
+
+The blast program used. This will be used to identify the source program in the
+second column of the GFF output file. Example of valid values include
+blastn, blastx, or wublast.
+
+=item -d,--database
+
+The name for the database that was blasted against. If provided, this
+will be appended to the program variable in the second colum of the 
+GFF output file.
+
+=item -m,--align
+
+The alignment format use in the BLAST report to be parsed.
+The program will assume that you are using the default alginment format for
+blast. Otherwise, you can specify 'tab' or '8' or '9' for tab delimited blast.
+
+=item -e,--maxe
+
+The maximum e value threshold to accept.
+
+=item -l,--min-len
+
+The minimum length to accept.
+
+=item --verbose
+
+Run the program with maximum reporting of error and status messages.
+
+=item --usage
+
+Short overview of how to use program from command line.
+
+=item --help
+
+Show program usage with summary of options.
+
+=item --version
+
+Show program version.
+
+=item --man
+
+Show the full program manual. This uses the perldoc command to print the 
+POD documentation for the program.
+
+=item -q,--quiet
+
+Run the program with minimal output.
+
+=back
+
+=head1 DIAGNOSTICS
+
+=over 2
+
+=item * Expecting input from STDIN
+
+If you see this message, it may indicate that you did not properly specify
+the input sequence with -i or --infile flag. 
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This program does not make use of a configuration file or any
+variables set in the user's environment.
+
+=head1 DEPENDENCIES
+
+=head2 Required Software
+
+This program requires output from a BLAST program. Since this program makes
+use of the BioPerl blast parser, it should be possible to convert
+local alignment results from any of the following programs:
+
+=over 2
+
+=item * BLAST
+
+=item * PSIBLAST
+
+=item * PSITBLASTN
+
+=item * RPSBLAST
+
+=item * WUBLAST
+
+=item * bl2seq
+
+=item * WU-BLAST
+
+=item * BLASTZ
+
+=item * BLAT
+
+=item * Paracel 
+
+=item * BTK
+
+=back
+
+=head2 Required Perl Modules
+
+The following perl modules are required for this program:
+
+=over 2
+
+=item * Bio::SearchIO
+
+The SearchIO module is part of the bioperl module.
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+=head2 Limitations
+
+=over
+
+=item * Limited BLAST testing
+
+This program has only been tested with BLAST output from the NCBI-BLAST package
+using standard and tab delimited output. If you find that there are limiations
+to this program that limit your use, please email the author or file a 
+bug report on the Sourceforge website:
+http://sourceforge.net/tracker/?group_id=204962
+
+=back
+
+=head1 LICENSE
+
+GNU General Public License, Version 3
+
+L<http://www.gnu.org/licenses/gpl.html>
+
+THIS SOFTWARE COMES AS IS, WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTY. USE AT YOUR OWN RISK.
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
 =head1 HISTORY
 
 STARTED: 08/06/2007
 
-UPDATED: 08/06/2007
+UPDATED: 01/30/2009
 
-VERSION: $Id: $
+VERSION: $Rev$
 
 =cut
 
@@ -372,3 +581,17 @@ VERSION: $Id: $
 #
 # 08/06/2007
 # -Program started
+# -Main body of program written
+# 01/30/2009
+# -Updated POD documentation
+# -Moved POD documentation to end of the program
+# -Updated to new print_help subfunction that extracts
+#  help from POD
+# -Added default to print to STDOUT if not outfile
+#  path given at command line
+# -Added default input from STDIN in no infile path
+#  given at command line
+# -Added package name DAWGPAWS
+# -Added options to pass database name, query sequence id,
+#  and program used. Otherwise these are attempted to
+#  be extracted from the command line
