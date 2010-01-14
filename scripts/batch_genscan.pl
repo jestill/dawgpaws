ef3d1b6fe257a67f613e35c7154736804e2875d4
James Estill <JamesEstill@gmail.com>
2010-01-14 15:53:11 +0000

    Updated batch_genscan.pl -Minor modifications, added gpl link

--- scripts/batch_genscan.pl
+++ scripts/batch_genscan.pl
@@ -19,6 +19,7 @@
 #  http://www.gnu.org/licenses/gpl.html                     |  
 #                                                           |
 #-----------------------------------------------------------+
+# minor modification
 
 #-----------------------------+
 # INCLUDES                    |
