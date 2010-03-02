076ec159e7bafb35313b9117f35229d0347584bd
James Estill <JamesEstill@gmail.com>
2010-03-02 17:52:33 +0000

    Updated cnv_repseek2gff.pl -Added support for GFF3 format output following SO

--- scripts/cnv_repseek2gff.pl
+++ scripts/cnv_repseek2gff.pl
@@ -8,7 +8,7 @@
 #  AUTHOR: James C. Estill                                  |
 # CONTACT: JamesEstill_@_gmail.com                          |
 # STARTED: 11/24/2008                                       |
-# UPDATED: 03/30/2009                                       |
+# UPDATED: 03/02/2010                                       |
 #                                                           |
 # DESCRIPTION:                                              |
 #  Convert repseek output to a GFF format compatible with   |
@@ -44,6 +44,8 @@ use File::Spec;                # Convert a relative path to an abosolute path
 # PROGRAM VARIABLES           |
 #-----------------------------+
 my ($VERSION) = q$Rev$ =~ /(\d+)/;
+# Get GFF version from environment, GFF2 is DEFAULT
+my $gff_ver = uc($ENV{DP_GFF}) || "GFF2";
 
 #-----------------------------+
 # VARIABLE SCOPE              |
@@ -68,10 +70,11 @@ my $do_test = 0;                  # Run the program in test mode
 #-----------------------------+
 # COMMAND LINE OPTIONS        |
 #-----------------------------+
-my $ok = GetOptions(# REQUIRED OPTIONS
+my $ok = GetOptions(# REQUIRED ARGUMENT
 		    "i|infile=s"  => \$infile,
                     "o|outfile=s" => \$outfile,
-		    # ADDITIONAL OPTIONS
+		    # OPTIONS
+		    "gff-ver=s"     => \$gff_ver,
 		    "s|seqname=s" => \$seq_id,
 		    "program=s"   => \$program,
 		    "p|param=s"   => \$parameter_set,
@@ -107,6 +110,25 @@ if ($show_version) {
     print "\ncnv_repseek2gff.pl:\n".
 	"Version: $VERSION\n\n";
     exit;
+}
+
+#-----------------------------+
+# STANDARDIZE GFF VERSION     |
+#-----------------------------+
+unless ($gff_ver =~ "GFF3" || 
+	$gff_ver =~ "GFF2") {
+    # Attempt to standardize GFF format names
+    if ($gff_ver =~ "3") {
+	$gff_ver = "GFF3";
+    }
+    elsif ($gff_ver =~ "2") {
+	$gff_ver = "GFF2";
+    }
+    else {
+	print "\a";
+	die "The gff-version \'$gff_ver\' is not recognized\n".
+	    "The options GFF2 or GFF3 are supported\n";
+    }
 }
 
 #-----------------------------+
@@ -188,11 +210,13 @@ sub repseek2gff {
 
 	my $copy1_start = $rep_parts[1];
 	my $copy2_start = $rep_parts[2];
+	my $copy1_len = $rep_parts[3];
+	my $copy2_len = $rep_parts[4];
 	my $copy1_end = $copy1_start + $rep_parts[3];
 	my $copy2_end = $copy2_start + $rep_parts[4];
+	my $copy_distance = $rep_parts[5];            # distance between the repeats
+	my $percent_identity = $rep_parts[7];   #  (matches / alignment_length)\n
 	my $alignment_score = $rep_parts[8];
-
-	my $attribute = "repseek".$repseek_id.":$repeat_direction";
 
 	# Will split the repseek source into inverted repeats 
 	# vs direct repeats .. putative TEs
@@ -201,9 +225,6 @@ sub repseek2gff {
 	# - Overlap
         # - Palindromes
 	my $feature;
-	#$source = "repseek";
-
-
 
 	# Translate repeat direction
 	if ($repeat_direction =~ "inv") {
@@ -214,24 +235,93 @@ sub repseek2gff {
 	}
 
 	if ($repeat_type =~ "Overlap") {
-	    $feature = "overlapping_"."$repeat_direction"."_repeat";
+	    #$feature = "overlapping_"."$repeat_direction"."_repeat";
+	    # overlapping repeats not recognized in SO
+	    $feature = $repeat_direction."_repeat";
 	}
 	else {
 	    $feature = "$repeat_direction"."_repeat";
 	}
 
 	if ($repeat_type =~ "Palindrome") {
-	    $feature = "palindromic_repeat";
+	    # palindromic repeats are currently not recognized
+	    # in the sequence ontology, will have to tag this
+	    # feature with additional information in the
+	    # attribute field.
+	    #$feature = "palindromic_repeat";
+	    $feature = "inverted_repeat";
 	}
 
-	# parameter set will go in the attribute column
+	my $attribute = "repseek".$repseek_id."_".$repeat_direction;
 
 	#-----------------------------+
 	# PRINT OUTPUT TO GFF         |
 	#-----------------------------+
+	my $parent_id;
+	if ($gff_ver =~ "GFF3") {
+
+	    #-----------------------------+
+	    # PRINT PARENT                |
+	    #-----------------------------+
+	    $parent_id = $attribute;
+	    my $parent_attribute = "ID=".$parent_id."_repeat";
+	    
+	    if ($repeat_type =~ "Palindrome") {
+		$parent_attribute = $parent_attribute.
+		    " ; Alias=palindrome";
+	    }
+	    elsif ($repeat_type =~ "Overlap") {
+		$parent_attribute = $parent_attribute.
+		    "; Alias=overlapping_direct_repeat";
+	    }
+	    elsif ($repeat_type =~ "Tandem") {
+		$parent_attribute = $parent_attribute.
+		    "; Alias=tandem_direct_repeat";
+	    }
+	    # Add not about other attribute
+	    $parent_attribute = $parent_attribute.
+		"; Note= ".$percent_identity.
+		" ".$copy1_len.
+		" ".$copy2_len.
+		" ".$copy_distance ;
+
+
+	    # GET SPAN LOCATIONS          |
+	    my @locations = ( int($copy1_start), 
+			      int($copy1_end), 
+			      int($copy2_start), 
+			      int($copy2_end) );
+	    @locations = sort @locations;
+	    my $span_start = $locations[0];
+	    my $span_end = $locations[3];
+
+	    # PRINT 
+	    print STDERR "#\n";
+	    print GFFOUT "$seqname\t".       # Seqname
+		"$source\t".                 # Source
+		"$feature\t".                # Feature
+		"$span_start\t".             # Start
+		"$span_end\t".               # End
+		"$alignment_score\t".        # Score
+		"+\t".                       # Strand
+		".\t".                       # Frame
+		"$parent_attribute".         # Attribute
+		"\n";
+
+	}
+	else {
+	    $attribute = $attribute
+	}
+
+
+	if ($gff_ver =~ "GFF3") {
+	    $attribute = "ID=".$parent_id."_copy1".
+		"; Parent=".$parent_id."_repeat";
+	    $feature = "repeat_unit";
+	}
 	print GFFOUT "$seqname\t".       # Seqname
 	    "$source\t".                 # Source
-	    "$feature\t".                    # Features: Apollo kluge
+	    "$feature\t".                # Feature
 	    "$copy1_start\t".            # Start
 	    "$copy1_end\t".              # End
 	    "$alignment_score\t".        # Score
@@ -240,9 +330,14 @@ sub repseek2gff {
 	    "$attribute".                # Attribute
 	    "\n";
 
+	if ($gff_ver =~ "GFF3") {
+	    $attribute = "ID=".$parent_id."_copy2".
+		"; Parent=".$parent_id."_repeat";
+	    $feature = "repeat_unit";
+	}
 	print GFFOUT "$seqname\t".       # Seqname
 	    "$source\t".                 # Source
-	    "$feature\t".                    # Features: Apollo kluge
+	    "$feature\t".                # Feature
 	    "$copy2_start\t".            # Start
 	    "$copy2_end\t".              # End
 	    "$alignment_score\t".        # Score
@@ -322,6 +417,23 @@ sub print_help {
    
 }
 
+sub seqid_encode {
+    # Following conventions for GFF3 v given at http://gmod.org/wiki/GFF3
+    # Modified from code for urlencode in the perl cookbook
+    # Ids must not contain unescaped white space, so spaces are not allowed
+    my ($value) = @_;
+    $value =~ s/([^[a-zA-Z0-9.:^*$@!+_?-|])/"%" . uc(sprintf "%lx" , unpack("C", $1))/eg;
+    return ($value);
+}
+
+sub gff3_encode {
+    # spaces are allowed in attribute, but tabs must be escaped
+    my ($value) = @_;
+    $value =~ s/([^[a-zA-Z0-9.:^*$@!+_?-| ])/"%" . uc(sprintf "%lx" , unpack("C", $1))/eg;
+    return ($value);
+}
+
+
 1;
 __END__
 
@@ -370,6 +482,14 @@ will write output to STDOUT.
 =head1 OPTIONS
 
 =over 2
+
+=item --gff-ver
+
+The GFF version for the output. This will accept either gff2 or gff3 as the
+options. By default the GFF version will be GFF2 unless specified otherwise.
+The default GFF version for output can also be set in the user environment
+with the DP_GFF option. The command line option will always override the option
+defined in the user environment.
 
 =item -s,--seqname
 
@@ -501,7 +621,7 @@ James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
 STARTED: 11/25/2008
 
-UPDATED: 03/30/2009
+UPDATED: 03/02/2010
 
 VERSION: $Rev$
 
@@ -533,3 +653,42 @@ VERSION: $Rev$
 #      -palindromic_repeat
 #  - Added support for --param
 #  - Added support for --program
+# 03/02/2010
+#  - Added support for gff3 output
+#
+# REPSEEK OUTPUT
+# Overlap.dir     117     141     48      52      -24     119-143-20-3.05 84.615  33.06   2.48    2       0.52
+#Distant.dir     3980    10979   1745    1731    5254    5141-12128-20-2.78      96.167  1593.04 7.03    7       0.67
+#Distant.dir     3980    13884   1749    1750    8155    3980-13884-57-5.88      86.969  1254.62 7.14    7       0.57
+#Distant.dir     4592    72427   459     471     67376   4764-72599-20-4.47      63.559  241.67  6.82    8       0.36
+#Distant.dir     10957   13867   1767    1776    1143    10985-13890-46-6.07     85.271  1206.85 6.92    7       0.65
+#Distant.dir     14494   72427   387     388     57546   14666-72599-20-4.47     58.763  187.50  6.90    8       0.38
+#Close.dir       18272   18347   35      35      40      18274-18349-33-2.00     97.143  32.56   2.00    2       1.00
+#Distant.dir     28066   41668   149     149     13453   28099-41701-56-2.99     97.987  142.99  2.51    2       0.51
+#Overlap.dir     29924   29954   99      99      -69     29949-29979-23-2.33     87.879  77.74   2.70    3       0.70
+#Distant.dir     32892   39731   1747    1749    5092    33404-40245-96-2.07     97.086  1633.51 6.60    7       0.53
+#Distant.dir     32892   79038   364     363     45782   32905-79051-30-4.20     84.110  241.70  6.96    7       0.98
+#Distant.dir     32894   71717   825     810     37998   32906-71729-70-5.90     70.192  423.29  6.79    7       0.54
+#Distant.dir     33585   72890   6510    6511    32795   38931-78243-59-2.51     92.172  5330.09 3.97    3       0.57
+#                       "\tRepeats are displayed in 12 COLUMNS\n"
+#                       "\t 01 - type of the repeat (Tandem|Close|Distant|Overlap|Palindrome|Interseq).(dir|inv)\n"
+#                       "\t        'Tandem'     : repeat is a perfect tandem (spacer=0)\n"
+#                       "\t        'Close'      : spacer is smaller than 1 kb\n"
+#                       "\t        'Distant'    : spacer is larger  than 1 kb\n"
+#                       "\t        'Overlap'    : repeat is composed of two overlapping copies (spacer<0)\n"
+#                       "\t        'Palindrome' : repeat is a perfect palindrome (spacer=0)\n"
+#                       "\t        'Interseq'   : repeat is shared by two sequences\n"
+#                       "\t 02 - position of the first copy\n"
+#                       "\t 03 - position of the second copy\n"
+#                       "\t 04 - length of the first copy\n"
+#                       "\t 05 - length of the second copy\n"
+#                       "\t 06 - spacer of the repeats (corrected when the sequence is circular)\n"
+#                       "\t 07 - characteristics of the seed that gave that repeat (pos1-pos2-len-meanR)\n"
+#                       "\t 08 - %%identity between the two copies (matches / alignment_length)\n"
+#                       "\t 09 - score of the alignement\n"
+#                       "\t 10 - mean-R of the repeat\n"
+#                       "\t 11 - mode-R of the repeat\n"
+#                       "\t 12 - fraction of its mode-R\n"
+#
+#                                );
+#
