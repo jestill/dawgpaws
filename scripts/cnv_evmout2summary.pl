9cb0e2b14003989c7c71daa5732e4b0a22294364
James Estill <JamesEstill@gmail.com>
2012-01-27 20:16:23 +0000

    Added cnv_evmout2summary.pl -Generate a simple tab delim text file of the evidences used to build gene models

---
+++ scripts/cnv_evmout2summary.pl
@@ -0,0 +1,369 @@
+#!/usr/bin/perl -w
+#-----------------------------------------------------------+
+#                                                           |
+# cnv_evmout2summary.pl - Summarize EVM evidences used      |
+#                                                           |
+#-----------------------------------------------------------+
+#                                                           |
+#  AUTHOR: James C. Estill                                  |
+# CONTACT: JamesEstill_@_gmail.com                          |
+# STARTED: 01/27/2012                                       |
+# UPDATED: 01/27/2012                                       |
+#                                                           |
+# DESCRIPTION:                                              |
+#  Summarize the evidences used by EVM to generate gene     |
+#  models. The purpose is to generate a tab delimited file  |
+#  that can be loaded to a database for further processing. |
+#                                                           |
+# USAGE:                                                    |
+#  cnv_evmout2summary.pl -i evmFile.evm.out -o summ.txt     |
+#                                                           |
+# VERSION: $Rev$                                            |
+#                                                           |
+#                                                           |
+# LICENSE:                                                  |
+#  GNU General Public License, Version 3                    |
+#  http://www.gnu.org/licenses/gpl.html                     |  
+#                                                           |
+#-----------------------------------------------------------+
+
+package DAWGPAWS;
+
+#-----------------------------+
+# INCLUDES                    |
+#-----------------------------+
+use strict;
+use Getopt::Long;
+# The following needed for printing help
+use Pod::Select;               # Print subsections of POD documentation
+use Pod::Text;                 # Print POD doc as formatted text file
+use IO::Scalar;                # For print_help subfunction
+use IO::Pipe;                  # Pipe for STDIN, STDOUT for POD docs
+use File::Spec;                # Convert a relative path to an abosolute path
+
+#-----------------------------+
+# PROGRAM VARIABLES           |
+#-----------------------------+
+my ($VERSION) = q$Rev$ =~ /(\d+)/;
+# Get GFF version from environment, GFF2 is DEFAULT
+my $gff_ver = uc($ENV{DP_GFF}) || "GFF2";
+
+#-----------------------------+
+# VARIABLE SCOPE              |
+#-----------------------------+
+my $infile;
+my $outfile;
+
+# BOOLEANS
+my $quiet = 0;
+my $verbose = 0;
+my $show_help = 0;
+my $show_usage = 0;
+my $show_man = 0;
+my $show_version = 0;
+my $do_test = 0;                  # Run the program in test mode
+
+#-----------------------------+
+# COMMAND LINE OPTIONS        |
+#-----------------------------+
+my $ok = GetOptions(# REQUIRED OPTIONS
+		    "i|infile=s"  => \$infile,
+                    "o|outfile=s" => \$outfile,
+		    # ADDITIONAL OPTIONS
+		    "gff-ver=s"   => \$gff_ver,
+		    "q|quiet"     => \$quiet,
+		    "verbose"     => \$verbose,
+		    # ADDITIONAL INFORMATION
+		    "usage"       => \$show_usage,
+		    "test"        => \$do_test,
+		    "version"     => \$show_version,
+		    "man"         => \$show_man,
+		    "h|help"      => \$show_help,);
+
+
+#-----------------------------+
+# PRINT REQUESTED HELP        |
+#-----------------------------+
+if ( ($show_usage) ) {
+#    print_help ("usage", File::Spec->rel2abs($0) );
+    print_help ("usage", $0 );
+}
+
+if ( ($show_help) || (!$ok) ) {
+#    print_help ("help",  File::Spec->rel2abs($0) );
+    print_help ("help",  $0 );
+}
+
+if ($show_man) {
+    # User perldoc to generate the man documentation.
+    system ("perldoc $0");
+    exit($ok ? 0 : 2);
+}
+
+if ($show_version) {
+    print "\nbatch_mask.pl:\n".
+	"Version: $VERSION\n\n";
+    exit;
+}
+
+#-----------------------------+
+# MAIN PROGRAM BODY           |
+#-----------------------------+
+if ($infile) {
+    open (INFILE, "<$infile") ||
+	die "Can not open input file\n";
+}
+else {
+    open (INFILE, "<&STDIN") ||
+	die "Can not open STDINT for input\b";
+}
+
+if ($outfile) {
+    open (OUTFILE, ">$outfile") ||
+	die "Can not open output file\n";
+}
+else {
+    open (OUTFILE, ">&STDOUT") ||
+	die "Can not write to STDOUT\n";
+}
+
+# ITERATE ACROSS THE INFILE
+my $evm_model_count = 0;
+while (<INFILE>) {
+    chomp;
+    
+    if ( m /^\#/ ) {
+	$evm_model_count++;
+#	print STDERR "NEW MODEL $evm_model_count\n";
+
+	# Can parse model information here before moving on
+	next;
+    }
+
+    print STDERR $_."\n" if $verbose;
+
+    my @evm_parts = split /\t/;
+    my $num_parts = @evm_parts;
+
+    if ($num_parts == 6) {
+	my @evidences = split(/\,/,$evm_parts[5]);
+	foreach my $evidence ( @evidences ) {
+	    # Remove brackets
+	    $evidence =~ s/\{//;
+	    $evidence =~ s/\}//;
+	    my ($model, $program) = split (/\;/, $evidence);
+
+	    print OUTFILE "evm".$evm_model_count."\t".
+		$program."\n";
+	}
+
+    }
+
+}
+
+print STDERR $evm_model_count." models processed\n";
+
+exit 0;
+
+#-----------------------------------------------------------+ 
+# SUBFUNCTIONS                                              |
+#-----------------------------------------------------------+
+
+sub print_help {
+    my ($help_msg, $podfile) =  @_;
+    # help_msg is the type of help msg to use (ie. help vs. usage)
+    
+    print "\n";
+    
+    #-----------------------------+
+    # PIPE WITHIN PERL            |
+    #-----------------------------+
+    # This code made possible by:
+    # http://www.perlmonks.org/index.pl?node_id=76409
+    # Tie info developed on:
+    # http://www.perlmonks.org/index.pl?node=perltie 
+    #
+    #my $podfile = $0;
+    my $scalar = '';
+    tie *STDOUT, 'IO::Scalar', \$scalar;
+    
+    if ($help_msg =~ "usage") {
+	podselect({-sections => ["SYNOPSIS|MORE"]}, $0);
+    }
+    else {
+	podselect({-sections => ["SYNOPSIS|ARGUMENTS|OPTIONS|MORE"]}, $0);
+    }
+
+    untie *STDOUT;
+    # now $scalar contains the pod from $podfile you can see this below
+    #print $scalar;
+
+    my $pipe = IO::Pipe->new()
+	or die "failed to create pipe: $!";
+    
+    my ($pid,$fd);
+
+    if ( $pid = fork() ) { #parent
+	open(TMPSTDIN, "<&STDIN")
+	    or die "failed to dup stdin to tmp: $!";
+	$pipe->reader();
+	$fd = $pipe->fileno;
+	open(STDIN, "<&=$fd")
+	    or die "failed to dup \$fd to STDIN: $!";
+	my $pod_txt = Pod::Text->new (sentence => 0, width => 78);
+	$pod_txt->parse_from_filehandle;
+	# END AT WORK HERE
+	open(STDIN, "<&TMPSTDIN")
+	    or die "failed to restore dup'ed stdin: $!";
+    }
+    else { #child
+	$pipe->writer();
+	$pipe->print($scalar);
+	$pipe->close();	
+	exit 0;
+    }
+    
+    $pipe->close();
+    close TMPSTDIN;
+
+    print "\n";
+
+    exit 0;
+   
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Name.pl - Short program description. 
+
+=head1 VERSION
+
+This documentation refers to program version 0.1
+
+=head1 SYNOPSIS
+
+=head2 Usage
+
+    Name.pl -i InFile -o OutFile
+
+=head2 Required Arguments
+
+    --infile        # Path to the input file
+    --outfie        # Path to the output file
+
+=head1 DESCRIPTION
+
+This is what the program does
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -i,--infile
+
+Path of the input file.
+
+=item -o,--outfile
+
+Path of the output file.
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item --usage
+
+Short overview of how to use program from command line.
+
+=item --help
+
+Show program usage with summary of options.
+
+=item --version
+
+Show program version.
+
+=item --man
+
+Show the full program manual. This uses the perldoc command to print the 
+POD documentation for the program.
+
+=item -q,--quiet
+
+Run the program with minimal output.
+
+=back
+
+=head1 EXAMPLES
+
+The following are examples of how to use this script
+
+=head2 Typical Use
+
+This is a typcial use case.
+
+=head1 DIAGNOSTICS
+
+=over 2
+
+=item * Expecting input from STDIN
+
+If you see this message, it may indicate that you did not properly specify
+the input sequence with -i or --infile flag. 
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+Names and locations of config files
+environmental variables
+or properties that can be set.
+
+=head1 DEPENDENCIES
+
+Other modules or software that the program is dependent on.
+
+=head1 BUGS AND LIMITATIONS
+
+Any known bugs and limitations will be listed here.
+
+=head1 REFERENCE
+
+A manuscript is being submitted describing the DAWGPAWS program. 
+Until this manuscript is published, please refer to the DAWGPAWS 
+SourceForge website when describing your use of this program:
+
+JC Estill and JL Bennetzen. 2009. 
+The DAWGPAWS Pipeline for the Annotation of Genes and Transposable 
+Elements in Plant Genomes.
+http://dawgpaws.sourceforge.net/
+
+=head1 LICENSE
+
+GNU General Public License, Version 3
+
+L<http://www.gnu.org/licenses/gpl.html>
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
+
+=head1 HISTORY
+
+STARTED:
+
+UPDATED:
+
+VERSION: $Rev$
+
+=cut
+
+#-----------------------------------------------------------+
+# HISTORY                                                   |
+#-----------------------------------------------------------+
+#
